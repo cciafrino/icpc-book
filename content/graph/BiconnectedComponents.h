0239ee70672581ebbed46bfe1c3df0a09888b830
Letícia Freire Carvalho de Sousa <leticiafcs4@gmail.com>
2019-09-09 16:02:30 -0300

    Update BiconnectedComponents.h

--- content/graph/BiconnectedComponents.h
+++ content/graph/BiconnectedComponents.h
@@ -1,5 +1,5 @@
 /**
- * Author: Simon Lindholm
+ * Author: Simon Lindholm (adapted)
  * Date: 2017-04-17
  * License: CC0
  * Source: folklore
@@ -10,45 +10,58 @@
  *  i.e., not part of any cycle.
  * Time: O(E + V)
  * Status: tested during MIPT ICPC Workshop 2017
+ *          Working
  * Usage:
  *  int eid = 0; ed.resize(N);
  *  for each edge (a,b) {
  *    ed[a].emplace_back(b, eid);
- *    ed[b].emplace_back(a, eid++); }
- *  bicomps([\&](const vi\& edgelist) {...});
+ *    ed[b].emplace_back(a, eid++); 
+ *  }
  */
-#pragma once
+
+typedef vector<int> vi;
+typedef vector<vector<pair<int,int>>> vii;
 
 vector<int> num, st;
-vector<vector<pair<int,int>> ed;
+vii ed;
 int Time;
-template<class F>
-int dfs(int at, int par, F f) {
+
+int dfs(int at, int par,vector<vector<int>> &comps) {
 	int me = num[at] = ++Time, e, y, top = me;
 	for (auto &pa : ed[at]) if (pa.second != par) {
 		tie(y, e) = pa;
 		if (num[y]) {
 			top = min(top, num[y]);
-			if (num[y] < me)
+			if (num[y] < me){
 				st.push_back(e);
+			}
 		} else {
 			int si = st.size();
-			int up = dfs(y, e, f);
+			int up = dfs(y, e, comps);
 			top = min(top, up);
 			if (up == me) {
+				
 				st.push_back(e);
-				f(vi(st.begin() + si, st.end()));
+				comps.push_back(vector<int>());
+				for(int i=st.size()-1;i>=si;i--){
+					comps[comps.size()-1].push_back(st[i]);
+				}
 				st.resize(si);
+				cont_comp++;
 			}
-			else if (up < me) st.push_back(e);
-			else { /* e is a bridge */ }
+			else if (up < me){ st.push_back(e);}
+			else { cont_comp++;comps.push_back({e});/* e is a bridge */ }
 		}
 	}
 	return top;
 }
 
-template<class F>
-void bicomps(F f) {
+
+vector<vector<int>> bicomps() { 
+	// returns components and its edges ids
+	vector<vector<int>> comps;
 	num.assign(ed.size(), 0);
-	for (int i = 0; i < ed.size(); ++i) if (!num[i]) dfs(i, -1, f);
+	for (int i = 0; i < ed.size(); ++i) 
+		if (!num[i]) dfs(i, -1, comps);
+	return comps;
 }
