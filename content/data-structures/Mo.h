16727627b01d91d20a288085b5378df1631938ee
Ciafrino <chrisciafrino@ufrj.br>
2019-11-16 19:35:34 -0300

    Fix

--- content/data-structures/Mo.h
+++ content/data-structures/Mo.h
@@ -5,37 +5,36 @@
  * Source: folklore
  * Description: Mo's algorithm example problem: Count how many elements appear at least two times in given range $[l, r]$.
  * Time: $(n + q)sqrt(n)$
-
+ * Status: Tested on SPOJ DQUERY
  */
-
 
 struct query_t {
 	int l, r, id;
 };
 
-int n, q, total; // elements, queries, result.
-const int sqn = sqrt(n);
-vector<int> values(n), freq(n), result(m);
-vector<query_t> queries(q);
+int n, m, total = 0; // elements, queries, result.
+const int sqn = sqrt(n), maxv = 1000000;
+vector<int> values(n), freq(2*maxv), result(m);
+vector<query_t> queries(m);
 
 sort(queries.begin(), queries.end(), [sqn](const query_t &a, const query_t &b) {
 	if (a.l/sqn != b.l/sqn) return a.l < b.l;
 	return a.r < b.r;
 });
 
-int lx = 0, r = -1;
+int l = 0, r = -1;
 for(query_t &q : queries) {
 	auto add = [&](int i) {
 		// Change if needed
-		freq[values[i]]++;
+		++freq[values[i]];
 		if (freq[values[i]] == 2) total += 2;
-		else if (freq[values[i]] > 2) total++;
+		else if (freq[values[i]] > 2) ++total;
 	}
 	auto del = [&](int i) {
 		// Change if needed
-		freq[values[i]]--;
+		--freq[values[i]];
 		if (freq[values[i]] == 1) total -= 2;
-		else if (freq[values[i]] > 1) total--;
+		else if (freq[values[i]] > 1) --total;
 	}
 	while(r < q.r) add(++r);
 	while(l > q.l) add(--l);
