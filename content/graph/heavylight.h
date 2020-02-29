328475b856fc336c76dc2a6e3fa515ee03e6f3dd
Ciafrino <chrisciafrino@ufrj.br>
2020-02-29 19:12:51 -0300

    Updating HLD

--- content/graph/heavylight.h
+++ content/graph/heavylight.h
@@ -1,102 +1,89 @@
 /**
- * Status: Tested at 2018 ACM-ICPC Brazil Subregional - L, 
- * CodeForces gym101807 - J
+ * Status: tested on codeForces 101807J and 101908L
  * Description: 
  * Time: 
  */
- 
- 
-typedef lint F;
-typedef vector<vector<pair<int,F>>> adj;
-F zero=0;
-struct HLD{
- 
-	adj g;
-	vector<int> in, par, parflow, sz, depth;
-	vector<F> val;
- 
-	//declare query struct
- 
- 
+#include "SegTree.h"
+typedef vector<vector<pair<int,int>>> adj;
+
+template<typename T, bool USE_EDGES>
+struct HLD {
 	int t, n;
-	bool USE_EDGES;
- 
+	vector<int> in, par, parflow, sz, depth;
+	vector<T> val;
+ 	tree_t<T> seg;
 	HLD(){}
- 
-	HLD(adj &g, bool u, int r = 0){init(g,u,r);}
- 
-	void init(adj &G, bool u, int r = 0){
-		t=0, g=G, n=g.size(), USE_EDGES = u;
-		par = parflow = vector<int>(n,-1);
-		depth = in = sz = vector<int>(n,0);
-		val.resize(n); val[0] = zero;
+	HLD(adj &g, int r = 0) : t(0), n(g.size()), par(n,-1), 
+	parflow(n,-1), depth(n), in(n), sz(n), val(n) { 
 		par[r] = parflow[r] = r;
-		dfs_sz(r);
-		dfs_flow(r);
-		strinit();
+		dfs_sz(g, r), dfs_flow(g, r); 
+		seg.s = val;
 	}
- 
-	void strinit(){
- 
+	T query(int a, int b) {//inclusive query
+		return seg.query(a, b+1);
 	}
-	F strquery(int a, int b){//inclusive query
- 
+	void update(int a, T value) {
+		seg.update(a, value);
 	}
-	void strupdate(int a, F value){
- 
+	void update(int a, int b, T value) {//inclusive update
+		
 	}
-	void strupdate(int a, int b, F value){//inclusive update
- 
-	}
- 
-	void dfs_sz(int u){ //par, size & depth
+	void dfs_sz(adj &g, int u) {
 		sz[u]++;
-		for(auto &e: g[u]){
+		for(auto &e : g[u]) {
 			int v = e.st;
-			if(par[v] == -1){
+			if(par[v] == -1) {
 				par[v] = u; depth[v] = depth[u] + 1;
-				dfs_sz(v);
+				dfs_sz(g, v);
 				sz[u] += sz[v]; 
-				if(par[u] == g[u][0].st || sz[v] > sz[g[u][0].st])
-					swap(g[u][0],e);
+				if (par[u] == g[u][0].st || sz[v] > sz[g[u][0].st])
+					swap(g[u][0], e);
 			}
 		}
 	}
- 
-	void dfs_flow(int u){//parflow, in & val
+	void dfs_flow(adj &g, int u) {//parflow, in & val
 		in[u] = t++;
-		for(auto &e:g[u]){
+		for (auto &e : g[u]) {
 			int v = e.st;
-			if(parflow[v] == -1){
+			if (parflow[v] == -1) {
 				if(e == g[u][0]) parflow[v] = parflow[u];
 				else parflow[v] = v;
-				dfs_flow(v);
-				val[in[v]] = USE_EDGES?e.nd:zero;
+				dfs_flow(g, v);
+				val[in[v]] = USE_EDGES ? e.nd : 0;
 			}
 		}
 	}
- 
-	F query(int u, int v){ //query path
-		if(u == v) return strquery(in[u],in[u]);
-		F ans = zero;	
-		for(int e, p; parflow[u] != parflow[v]; u=p){
+	void update_path(int u, int v, F value){ //update path
+		if (u == v){ update(in[u],in[u],value); return; }
+		for(int e, p; parflow[u] != parflow[v]; u = p){
 			if(depth[parflow[u]] < depth[parflow[v]]) swap(u,v);
 			e = 1, p = parflow[u];
 			if(u == p) e = 0, p = par[u];
-			ans += strquery(in[parflow[u]]+e,in[u]);
+			update(in[parflow[u]] + e, in[u], value);
 		}
-		if(in[u] > in[v]) swap(u,v);
-		return ans += strquery(in[u]+USE_EDGES,in[v]);
+		if (in[u] > in[v]) swap(u, v);
+		update(in[u] + USE_EDGES, in[v], value);
 	}
-	void update(int u, int v, F value){ //update path
-		if(u == v){strupdate(in[u],in[u],value);return;}
-		for(int e, p; parflow[u] != parflow[v]; u=p){
+	T query_path(int u, int v) {
+		if (u == v) return query(in[u], in[u]);
+		T ans = T();	
+		for (int e, p; parflow[u] != parflow[v]; u = p){
 			if(depth[parflow[u]] < depth[parflow[v]]) swap(u,v);
 			e = 1, p = parflow[u];
 			if(u == p) e = 0, p = par[u];
-			strupdate(in[parflow[u]]+e,in[u],value);
+			ans += query(in[parflow[u]] + e, in[u]);
 		}
-		if(in[u] > in[v]) swap(u,v);
-		strupdate(in[u]+USE_EDGES,in[v],value);
+		if (in[u] > in[v]) swap(u, v);
+		return ans += query(in[u] + USE_EDGES, in[v]);
+	}	
+	void update_node(int u, int v, T value) {
+		if (depth[u] < depth[v]) u = v;
+		update(in[u],value);
+	}
+	int querySubtree(int v) { 
+		return tree.query(in[v] + USE_EDGES, pos[v] + sz[v]);
+	}
+	void modifySubtree(int v, T value) {
+		tree.update(in[v] + VALS_EDGES, in[v] + sz[v], value);
 	}
 };
