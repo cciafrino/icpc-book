0a7deb3c3fa41e52675d79840243dcb91cd0e500
Letícia Freire Carvalho de Sousa <leticiafcs4@gmail.com>
2020-02-29 21:13:34 -0300

    Update heavylight.h

--- content/graph/heavylight.h
+++ content/graph/heavylight.h
@@ -9,13 +9,13 @@ typedef vector<vector<pair<int,int>>> adj; //vertex, value
 template<typename T, bool USE_EDGES>
 struct HLD {
 	int t, n;
-	vector<int> in, par, parflow, sz, depth;
+	vector<int> in, par, pf, sz, dep;
 	vector<T> val;
  	tree_t<T> seg; //or another query struct
 	HLD(){}
 	HLD(adj &g, int r = 0) : t(0), n(g.size()), par(n,-1), 
-	parflow(n,-1), depth(n), in(n), sz(n), val(n) { 
-		par[r] = parflow[r] = r;
+	pf(n,-1), dep(n), in(n), sz(n), val(n) { 
+		par[r] = pf[r] = r;
 		dfs_sz(g, r), dfs_flow(g, r); 
 		seg.s = val; //init query struct
 	}
@@ -28,12 +28,12 @@ struct HLD {
 	void update(int a, int b, T value) {//inclusive update
 		
 	}
-	void dfs_sz(adj &g, int u) { //par, size & depth
+	void dfs_sz(adj &g, int u) { //par, size & dep
 		sz[u]++;
 		for(auto &e : g[u]) {
 			int v = e.st;
 			if(par[v] == -1) {
-				par[v] = u; depth[v] = depth[u] + 1;
+				par[v] = u; dep[v] = dep[u] + 1;
 				dfs_sz(g, v);
 				sz[u] += sz[v]; 
 				if (par[u] == g[u][0].st || sz[v] > sz[g[u][0].st])
@@ -41,13 +41,13 @@ struct HLD {
 			}
 		}
 	}
-	void dfs_flow(adj &g, int u) {//parflow, in & val
+	void dfs_flow(adj &g, int u) {//pf, in & val
 		in[u] = t++;
 		for (auto &e : g[u]) {
 			int v = e.st;
-			if (parflow[v] == -1) {
-				if(e == g[u][0]) parflow[v] = parflow[u];
-				else parflow[v] = v;
+			if (pf[v] == -1) {
+				if(e == g[u][0]) pf[v] = pf[u];
+				else pf[v] = v;
 				dfs_flow(g, v);
 				val[in[v]] = USE_EDGES ? e.nd : T();
 			}
@@ -55,11 +55,11 @@ struct HLD {
 	}
 	void update_path(int u, int v, T value){ //update path
 		if (u == v){ update(in[u],in[u],value); return; }
-		for(int e, p; parflow[u] != parflow[v]; u = p){
-			if(depth[parflow[u]] < depth[parflow[v]]) swap(u,v);
-			e = 1, p = parflow[u];
+		for(int e, p; pf[u] != pf[v]; u = p){
+			if(dep[pf[u]] < dep[pf[v]]) swap(u,v);
+			e = 1, p = pf[u];
 			if(u == p) e = 0, p = par[u];
-			update(in[parflow[u]] + e, in[u], value);
+			update(in[pf[u]] + e, in[u], value);
 		}
 		if (in[u] > in[v]) swap(u, v);
 		update(in[u] + USE_EDGES, in[v], value);
@@ -67,17 +67,17 @@ struct HLD {
 	T query_path(int u, int v) {
 		if (u == v) return query(in[u], in[u]);
 		T ans = T();	
-		for (int e, p; parflow[u] != parflow[v]; u = p){
-			if(depth[parflow[u]] < depth[parflow[v]]) swap(u,v);
-			e = 1, p = parflow[u];
+		for (int e, p; pf[u] != pf[v]; u = p){
+			if(dep[pf[u]] < dep[pf[v]]) swap(u,v);
+			e = 1, p = pf[u];
 			if(u == p) e = 0, p = par[u];
-			ans += query(in[parflow[u]] + e, in[u]);
+			ans += query(in[pf[u]] + e, in[u]);
 		}
 		if (in[u] > in[v]) swap(u, v);
 		return ans += query(in[u] + USE_EDGES, in[v]);
 	}	
 	void update_node(int u, int v, T value) {
-		if (depth[u] < depth[v]) u = v;
+		if (dep[u] < dep[v]) u = v;
 		update(in[u],value);
 	}
 	T querySubtree(int v) { 
