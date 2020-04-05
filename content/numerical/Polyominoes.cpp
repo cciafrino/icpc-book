0456fdabb2d4fb0d2914b8f74862a812dd89d5cb
Letícia Freire Carvalho de Sousa <leticiafcs4@gmail.com>
2020-04-05 12:20:57 -0300

    Small Improvements

--- content/numerical/Polyominoes.cpp
+++ content/numerical/Polyominoes.cpp
@@ -2,36 +2,36 @@
  * Author: LeticiaFCS
  * Date: 2020-04-01
  * License: CC0
- * Description: Generate all free polyominoes with lim squares 
+ * Description: Generate all free polyominoes with n squares 
  * Time: bad
  * Status: Tested on URI 1712
  */
-
 int numdir = 4;
-int diri[] = {0,1,0,-1};
-int dirj[] = {1,0,-1,0};
+int diri[] = { 0, 1, 0,-1};
+int dirj[] = { 1, 0,-1, 0};
 
-set< set<pair<int,int>> > poly [LIM];
-void findPoly(int lim){
-	poly[1] = {{{0,0}}};
-	for(int i=2;i<=lim;i++){
-		for(auto om : poly[i-1]){
-			auto o = om;
-			for(auto pos: om){
-				for(int d=0;d<numdir;d++){
-					int x=pos.st+diri[d], y=pos.nd+dirj[d];
-					if(!om.count({x,y})){
-						o.insert({x,y});
-						int xn = (*o.begin()).first;
-						int yn = (*o.begin()).second;
-						set<pair<int,int>> norm;
-						for(auto posn:o)
-							norm.insert({posn.first-xn, posn.second-yn});
-						if(!poly[i].count(norm)) poly[i].insert(norm);
-						o.erase({x,y});
+set< set< pair<int,int> > > poly [LIM];
+void findPoly(int n){
+	poly[1] = { { { 0 , 0 } } };
+	for(int i = 2 ; i <= n; i++)
+		for(auto &om : poly[i-1]){
+			pair<int,int> mini =
+			{om.begin() -> st, om.begin() -> nd};
+			for(auto &p: om){
+				for(int d = 0; d < numdir; d++){
+					int x = p.st + diri[d];
+					int y = p.nd + dirj[d];
+					if(! om.count({x,y}) ){
+						auto m = min(mini, {x,y});
+						set<pair<int,int> > norm;
+						for(auto &pn:om)
+							norm.insert({pn.st - m.st,
+								     pn.nd - m.nd});
+						norm.insert({x - m.st, y - m.nd});
+						if(! poly[i].count(norm) )
+							poly[i].insert(norm);
 					}
 				}
 			}
 		}
-	}
 }
