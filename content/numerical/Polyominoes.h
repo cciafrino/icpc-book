afd74e934c8755ca51802fbac4bed9bd69b5911f
Ciafrino <chrisciafrino@ufrj.br>
2020-04-05 21:14:25 -0300

    Update Polyominoes.h

--- content/numerical/Polyominoes.h
+++ content/numerical/Polyominoes.h
@@ -12,23 +12,23 @@ int diri[] = { 0, 1, 0,-1};
 int dirj[] = { 1, 0,-1, 0};
 
 typedef pair<int,int> pii;
-vector< vector<pii> > poly [LIM];
+vector<vector<pii>> poly [LIM];
 void findPoly(int n){
-	poly[1] = { { { 0 , 0 } } };
-	for(int i = 2 ; i <= n; i++){
-		set< vector<pii> > cur_om;
-		for(auto &om : poly[i-1]){
+	poly[1] = {{{0 , 0}}};
+	for(int i = 2 ; i <= n; i++) {
+		set<vector<pii>> cur_om;
+		for(auto &om : poly[i-1]) {
 			pii mini = om[0];
-			for(auto &p: om)
-				for(int d = 0; d < 4; d++){
+			for(auto &p : om)
+				for(int d = 0; d < 4; d++) {
 					int x = p.st + diri[d];
 					int y = p.nd + dirj[d];
-					if(! binary_search(om.begin(), om.end(), pii(x,y)) ){
-						auto m = min(mini, {x,y});
-						pii new_cell(x-m.st, y-m.nd);
+					if(!binary_search(om.begin(), om.end(), pii(x,y))) {
+						auto m = min(mini, {x, y});
+						pii new_cell(x - m.st, y - m.nd);
 						bool new_in = false;
 						vector<pii> norm;
-						for(auto &pn:om){
+						for(auto &pn : om) {
 							pii cur(pn.st - m.st, pn.nd - m.nd);
 							if(cur > new_cell && !new_in){
 								new_in = true;
