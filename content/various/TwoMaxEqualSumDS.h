eeebd6ca1030b86ef5cd1384e691c04050449ba1
Letícia Freire Carvalho de Sousa <leticiafcs4@gmail.com>
2020-07-03 18:01:40 -0300

    Update TwoMaxEqualSumDS.h

--- content/various/TwoMaxEqualSumDS.h
+++ content/various/TwoMaxEqualSumDS.h
@@ -6,17 +6,16 @@
  * in the first subset and s[i] = 2 if v[i] is in the
  * second subset
  * Status: tested on URI1700 and leetcode Tallest Billboard
- * Time: $O(n * max sum)$
+ * Time: $O(n * S)$
  */
 
 pair<int, vector<int>> twoMaxEqualSumDS(vector<int> &v){
-    int i, n = (int) v.size();
-    int sum = accumulate(v.begin(), v.end(), 0);
-    vector<int> dp(2*sum + 1, INT_MIN/2), newdp(2*sum + 1);
-    dp[sum] = 0;
+    const int n = (int) v.size(),
+    sum = accumulate(v.begin(), v.end(), 0);
+    vector<int> dp(2*sum + 1, INT_MIN/2), newdp(2*sum + 1), s(n);
     vector<vector<int>> rec(n, vector<int>(2*sum + 1));
-    vector<int> s(n);   
     
+    int i; dp[sum] = 0;   
     for(i=0; i<n; i++, swap(dp, newdp))
         for(int a, b, d = v[i]; d <= 2*sum - v[i]; d++){
             newdp[d] = max({dp[d], a = dp[d - v[i]] + v[i], b = dp[d + v[i]]});
@@ -28,4 +27,3 @@ pair<int, vector<int>> twoMaxEqualSumDS(vector<int> &v){
     
     return {dp[sum], s};
 }
-
