49d4f087765c9c388a89f39b46db9e69042828cc
ciafrino <chrisciafrino@ufrj.br>
2020-08-02 20:34:13 -0300

    Refactoring Bicon

--- content/graph/BiconnectedComponents.h
+++ content/graph/BiconnectedComponents.h
@@ -1,5 +1,5 @@
 /**
- * Author: Simon Lindholm (adapted)
+ * Author: Simon Lindholm
  * Date: 2017-04-17
  * License: CC0
  * Source: folklore
@@ -14,42 +14,37 @@
  *  int eid = 0; ed.resize(N);
  *  for each edge (a,b) {
  *    ed[a].emplace_back(b, eid);
- *    ed[b].emplace_back(a, eid++); 
- *  }
+ *    ed[b].emplace_back(a, eid++); }
+ *  bicomps([\&](const vi\& edgelist) {...});
  */
-typedef vector<vector<pair<int,int>>> vii;
 vector<int> num, st;
-vii ed;
+vector<vector<pii>> ed;
 int Time;
-int dfs(int at, int par,vector<vector<int>> &comps) {
+template<class F> int dfs(int at, int par, F& f) {
 	int me = num[at] = ++Time, e, y, top = me;
-	for (auto &pa : ed[at]) if (pa.second != par) {
+	for(auto &pa : ed[at]) if (pa.second != par) {
 		tie(y, e) = pa;
 		if (num[y]) {
 			top = min(top, num[y]);
 			if (num[y] < me) st.push_back(e);
 		} else {
-			int si = st.size();
-			int up = dfs(y, e, comps);
+			int si = int(st.size());
+			int up = dfs(y, e, f);
 			top = min(top, up);
 			if (up == me) {
 				st.push_back(e);
-				comps.push_back(vector<int>());
-				for(int i = st.size()-1;i >= si; --i)
-					comps[comps.size()-1].push_back(st[i]);
+				f(vector<int>(st.begin() + si, st.end()));
 				st.resize(si);
-				cont_comp++;
 			}
-			else if (up < me){ st.push_back(e);}
-			else { cont_comp++; comps.push_back({e});/* e is a bridge */ }
+			else if (up < me) st.push_back(e);
+			else { /* e is a bridge */ }
 		}
 	}
 	return top;
 }
-vector<vector<int>> solve() { // returns components 
-	vector<vector<int>> comps; // and its edges ids
+
+template<class F> void bicomps(F f) {
 	num.assign(ed.size(), 0);
-	for (int i = 0; i < ed.size(); ++i) 
-		if (!num[i]) dfs(i, -1, comps);
-	return comps;
+	for(int i = 0; i < int(ed.size()); ++i) 
+		if (!num[i]) dfs(i, -1, f);
 }
