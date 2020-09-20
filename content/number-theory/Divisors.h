de6693f3ac153deae086bf53e94c8d5c21779f8e
Letícia Freire Carvalho de Sousa <leticiafcs4@gmail.com>
2020-09-20 12:49:15 -0300

    Update Divisors.h

--- content/number-theory/Divisors.h
+++ content/number-theory/Divisors.h
@@ -7,7 +7,7 @@ vector<int> divisors(int N) {
     for (int d = 1; d*d <= N; ++d) 
         if (N % d == 0) {
             result.push_back(d);
-            if (N > i*i) result.push_back(N/i);
+            if (N > d*d) result.push_back(N/i);
         }
     return result;
 }
