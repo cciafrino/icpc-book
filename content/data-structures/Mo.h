29cef34e83570e27d12f75b6e8e1c42ca78fdd70
ciafrino <chrisciafrino@ufrj.br>
2020-09-28 00:49:03 -0300

    Adding new Wavelet Mat

--- content/data-structures/Mo.h
+++ content/data-structures/Mo.h
@@ -3,22 +3,23 @@
  * Date: 2020
  * License: CC0
  * Source: kactl
- * Description: Mo's algorithm example problem: Count how many elements appear at least two times in given range $[l, r]$.
+ * Description: Answer interval or tree path queries by finding an approximate TSP through the queries,
+ * and moving from one query to the next by adding/removing points at the ends. Mo's algorithm example problem: Count how many elements appear at least two times in given range $[l, r]$.
  * For path queries on trees, flatten the tree by DFSing and pushing even-depth nodes at entry and odd-depth nodes at exit.
  * Time: O(N \sqrt Q)
  * Status: Tested on SPOJ DQUERY and kattis farmingmars
  */
 template<typename T>
-vector<T> mo(vector<pair<int, int>>& Q, vector<int>& A) {
+vector<T> mo(vector<array<int,2>>& Q, vector<int>& A) {
 	const int sqn = 370; // ~ N/sqrt(Q)
 	vector<int> s(int(Q.size()));
 	iota(s.begin(), s.end(), 0);
-#define K(x) make_pair(x.first/sqn, x.second ^ -(x.first/sqn & 1))
+#define K(x) make_pair(x[0]/sqn, x[1] ^ -(x[0]/sqn & 1))
 	sort(s.begin(), s.end(), [&](int x, int y) { return K(Q[x]) < K(Q[y]); });
 	const int ma = 100100; // max value in freq table
 	vector<T> result(int(Q.size()));
 	vector<int> freq(ma+1);
-	int L = 0, R = -1;
+	int L = 0, R = 0;
 	T cur = 0;
 	for (auto& qi : s) {
 		auto q = Q[qi];
@@ -32,10 +33,10 @@ vector<T> mo(vector<pair<int, int>>& Q, vector<int>& A) {
 			if (freq[A[i]] == 1) cur -= 2;
 			else if (freq[A[i]] > 1) --cur;
 		};
-		while(R < q.second) add(++R);
-		while(L > q.first) add(--L);
-		while(R > q.second) del(R--);
-		while(L < q.first) del(L++);
+		while(R < q[1]) add(++R);
+		while(L > q[0]) add(--L);
+		while(R > q[1]) del(R--);
+		while(L < q[0]) del(L++);
 		result[qi] = cur; 
 	}
 	return result;
--- content/data-structures/Wavelet.h
+++ content/data-structures/Wavelet.h
@@ -1,35 +1,136 @@
 /**
- * Author: Chris + BenQ
+ * Author: Chris
  * Date: 
  * License: 
- * Source: BenQ
- * Description: Segment tree on values instead of indices. kth return the largest number in 0-indexed interval.
- * count return the number of elements of a[i, j) that belong in [x, y].
+ * Source: Yosupo
+ * Description: Segment tree on values instead of indices.
  * Time: $O(\log(n))$
  */
+struct wavelet_t {
+    struct BitVector { // space: 32N bits
+        vector<int> _rank = {0};
+        BitVector(vector<char> v = vector<char>()) {
+            _rank.reserve(v.size() + 1);
+            for (int d : v) _rank.push_back(_rank.back() + d);
+        }
+        int rank(bool f, int k) { return f ? _rank[k] : (k - _rank[k]); }
+        int rank(bool f, int l, int r) { return rank(f, r) - rank(f, l); }
+    };
+    /*struct BitVector {  // space: 1.5N bits
+        vector<ull> v;
+        vector<int> _rank;
+        BitVector(vector<char> _v = vector<char>()) {
+            int n = int(_v.size());
+            v = vector<ull>((n + 63) / 64);
+            _rank = vector<int>(v.size() + 1);
+            for (int i = 0; i < n; i++) {
+                if (_v[i]) {
+                    v[i / 64] |= 1ULL << (i % 64);
+                    _rank[i / 64 + 1]++;
+                }
+            }
+            for (int i = 0; i < int(v.size()); i++) {
+                _rank[i+1] += _rank[i];
+            }
+        }
+        int rank(int k) {
+            int a = _rank[k / 64];
+            if (k % 64) a += __builtin_popcountll(v[k / 64] << (64 - k % 64));
+            return a;
+        }
+        int rank(bool f, int k) { return f ? rank(k) : k - rank(k); }
+        int rank(bool f, int l, int r) { return rank(f, r) - rank(f, l); }
+    };*/
+    int n, lg = 1;
+    vector<int> mid;
+    vector<BitVector> data;
+    wavelet_t(vector<int> v = vector<int>()) : n(int(v.size())) {
+        int ma = 0;
+        for (int x : v) ma = max(ma, x);
+        while ((1 << lg) <= ma) lg++;
+        mid = vector<int>(lg);
+        data = vector<BitVector>(lg);
+        for (int lv = lg - 1; lv >= 0; lv--) {
+            vector<char> buf;
+            vector<vector<int>> nx(2);
+            for (int d : v) {
+                bool f = (d & (1 << lv)) > 0;
+                buf.push_back(f);
+                nx[f].push_back(d);
+            }
+            mid[lv] = int(nx[0].size());
+            data[lv] = BitVector(buf);
+            v.clear();
+            v.insert(v.end(), nx[0].begin(), nx[0].end());
+            v.insert(v.end(), nx[1].begin(), nx[1].end());
+        }
+    }
+    pair<int, int> succ(bool f, int a, int b, int lv) {
+        int na = data[lv].rank(f, a) + (f ? mid[lv] : 0);
+        int nb = data[lv].rank(f, b) + (f ? mid[lv] : 0);
+        return {na, nb};
+    }
+    // count i, s.t. (a <= i < b) && (v[i] < u)
+    int rank(int a, int b, int u) {
+        if ((1 << lg) <= u) return b - a;
+        int ans = 0;
+        for (int lv = lg - 1; lv >= 0; lv--) {
+            bool f = (u & (1 << lv)) > 0;
+            if (f) ans += data[lv].rank(false, a, b);
+            tie(a, b) = succ(f, a, b, lv);
+        }
+        return ans;
+    }
+    // k-th(0-indexed!) number in v[a..b]
+    int select(int a, int b, int k) {
+        int u = 0;
+        for (int lv = lg - 1; lv >= 0; lv--) {
+            int le = data[lv].rank(false, a, b);
+            bool f = (le <= k);
+            if (f) {
+                u += (1 << lv);
+                k -= le;
+            }
+            tie(a, b) = succ(f, a, b, lv);
+        }
+        return u;
+    }
+    // k-th(0-indexed!) largest number in v[a..b]
+    int large_select(int a, int b, int k) { 
+        return select(a, b, b - a - k - 1);
+    }
+    // count i s.t. (a <= i < b) && (x <= v[i] < y)
+    int count(int a, int b, int x, int y) {
+        return rank(a, b, y) - rank(a, b, x);
+    }
+    // max v[i] s.t. (a <= i < b) && (v[i] < x)
+    int pre_count(int a, int b, int x) {
+        int cnt = rank(a, b, x);
+        return cnt == 0 ? -1 : select(a, b, cnt - 1);
+    }
+    // min v[i] s.t. (a <= i < b) && (x <= v[i])
+    int nxt_count(int a, int b, int x) {
+        int cnt = rank(a, b, x);
+        return cnt == b - a ? -1 : select(a, b, cnt);
+    }
+};
 
-template<int SZ> struct Wavelet { 
-	vector<int> L[SZ], R[SZ];
-	void build(vector<int> &a, int v=1, int l=0, int r=SZ-1) { 
-		if (l == r) return;
-		L[v] = R[v] = {0};
-		vector<int> A[2]; int m = l + (r-l)/2;
-		for(auto &t : a) {
-			A[t>m].push_back(t);
-			L[v].push_back(A[0].size()), R[v].push_back(A[1].size());
-		}
-		build(A[0],2*v,l,m), build(A[1],2*v+1,m+1,r);
-	}
-	int kth(int i,int j,int k,int v=1,int l=0,int r=SZ-1) { // [i, j)!!
-		if (l == r) return l;
-		int m = l + (r - l)/2, t = L[v][j]-L[v][i];
-		if (t >= k) return kth(L[v][i], L[v][j],k,2*v,l,m);
-		return kth(R[v][i], R[v][j],k-t,2*v+1,m+1,r);
-	}
-	int count(int i,int j,int x, int y, int v=1,int l=0,int r=SZ-1) { 
-		if (y < l || r < x) return 0; //count(i, j, x, y) retorna o numero de elementos 
-		if (x <= l && r <= y) return j - i; // de a[i, j) que pertencem a [x, y]
-		int m = l + (r - l)/2;
-		return count(L[v][i], L[v][j], x, y, 2*v, l, m) + count(i-L[v][i], j-L[v][j], x, y, 2*v+1, m+1, r);
-	}
+struct CompressWavelet {
+    wavelet_t wt;
+    vector<int> v, vidx;
+    int zip(int x) {
+        return int(lower_bound(vidx.begin(), vidx.end(), x) - vidx.begin());
+    }
+    CompressWavelet(vector<int> _v = vector<int>()) : v(_v), vidx(v) {
+        sort(vidx.begin(), vidx.end());
+        vidx.erase(unique(vidx.begin(), vidx.end()), vidx.end());
+        for (auto& d : v) d = zip(d);
+        wt = Wavelet(v);
+    }
+    int rank(int a, int b, int u) { return wt.rank(a, b, zip(u)); }
+    int select(int a, int b, int k) { return vidx[wt.select(a, b, k)]; }
+    int largest(int a, int b, int k) { return wt.large_select(a, b, k); }
+    int count(int a, int b, int mi, int ma) { return wt.count(a, b, mi, ma); }
+    int find_max(int a, int b, int x) { return wt.pre_count(a, b, x); }
+    int find_min(int a, int b, int x) { return wt.nxt_count(a, b, x); }
 };
