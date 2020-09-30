044c252909f7f2bcd394cf136caf70c13dafef24
Leticia-Freire <leticiafcs4@gmail.com>
2020-09-30 15:44:51 -0300

    range color stress test

--- content/data-structures/RangeColor.h
+++ content/data-structures/RangeColor.h
@@ -4,10 +4,10 @@
  * License: 
  * Source: Paulo Miranda
  * Description: RangeColor structure, supports point queries and range updates
- * if $C$ isn't int32_t change $freq$ to map<C, int64_t>
+ * if $C$ isn't int32\_t change $freq$ to map
  * Status: Tested on URI 2698
  * Time: $O(\lg(L)*Q)$
- * Status: not well tested
+ * Status: stress-tested
  */
 
 template<class T = int64_t, class C = int32_t> struct RangeColor{
@@ -15,12 +15,12 @@ template<class T = int64_t, class C = int32_t> struct RangeColor{
 	struct Node{
 		T l, r;
 		C color;
-		bool operator<(const Node &n) const{return r < n.r;}
+		bool operator < (const Node &n) const{ return r < n.r; }
 	};
 	
 	C minInf;
 	set<Node> st;
-	vector<int64_t> freq;
+	vector<T> freq;
 	
 	RangeColor(T first, T last, C maxColor, C iniColor = 0, C minusInf = -1): minInf(minusInf), freq(maxColor + 1) {
 		freq[iniColor] = last - first + 1LL;
@@ -28,12 +28,12 @@ template<class T = int64_t, class C = int32_t> struct RangeColor{
 	}
 	//get color in position i
 	C query(T i){
-		auto p = st.upper_bound(Node({0, i - 1LL, minInf}));
+		auto p = st.upper_bound(Node( {0, i - 1LL, minInf} ));
 		return p->color;
 	}
 	//set newColor in [a, b]
 	void upd(T a, T b, C newColor){
-		auto p = st.upper_bound(Node({0, a - 1LL, minInf}));
+		auto p = st.upper_bound(Node( {0, a - 1LL, minInf} ));
 		assert(p != st.end());
 		T l = p->l, r = p->r;
 		C old = p->color;
@@ -41,11 +41,11 @@ template<class T = int64_t, class C = int32_t> struct RangeColor{
 		p = st.erase(p);
 		if (l < a){
 			freq[old] += (a - l);
-			st.insert(Node({l, a - 1LL, old}));
+			st.insert(Node( {l, a - 1LL, old} ));
 		}
 		if (b < r){
 			freq[old] += (r - b);
-			st.insert(Node({b + 1LL, r, old}));
+			st.insert(Node( {b + 1LL, r, old} ));
 		}
 		while ((p != st.end()) and (p->l <= b)){
 			l = p->l, r = p->r;
@@ -54,14 +54,14 @@ template<class T = int64_t, class C = int32_t> struct RangeColor{
 			if (b < r){
 				freq[old] += (r - b);
 				st.erase(p);
-				st.insert(Node({b + 1LL, r, old}));
+				st.insert(Node( {b + 1LL, r, old} ));
 				break;
 			}else{
 				p = st.erase(p);
 			}
 		}
 		freq[newColor] += (b - a + 1LL);
-		st.insert(Node({a, b, newColor}));
+		st.insert(Node( {a, b, newColor} ));
 	}
 	T countColor(C x){
 		return freq[x];
---
+++ stress-tests/data-structures/RangeColor.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#define MAXN 40000
+using namespace std;
+
+#include "../../content/data-structures/RangeColor.h"
+
+using lint = int64_t;
+
+int main(){
+	srand(time(NULL));
+	for(int _=0; _<10; _++){
+		lint l = rand()%MAXN;
+		lint r = rand()%MAXN;
+		tie(l, r) = minmax(l,r);
+		lint maxColor = rand()%MAXN;
+		lint iniColor = maxColor ? rand()%maxColor : 0;
+		
+		auto rangeStr = RangeColor<>(l, r, maxColor, iniColor);
+		
+		vector<int> color(r + 1, iniColor);
+		vector<int> freq(maxColor + 1);
+		freq[iniColor] = (r - l + 1);
+		
+		int q = rand()%MAXN;
+		while(q--){
+			int type = rand()%3;
+			if(type == 0){ // query
+				int i = l + rand()%(r - l + 1);
+				assert(rangeStr.query(i) == color[i]);
+			}else if(type == 1){//upd
+				lint a = l + rand()%(r - l + 1);
+				lint b = l + rand()%(r - l + 1);
+				tie(a, b) = minmax(a, b);
+				int c = maxColor ? rand()%maxColor : 0;
+				
+				for(int i=a; i<=b; i++){
+					freq[color[i]]--;
+					color[i] = c;
+					freq[color[i]]++;
+				}
+				rangeStr.upd(a, b, c);
+			}else{ //countColor
+				int c = maxColor ? rand()%maxColor : 0;
+				assert(freq[c] == rangeStr.countColor(c));
+			}
+		
+		}
+		
+		
+		
+	}
+	
+	
+	return 0;
+}
