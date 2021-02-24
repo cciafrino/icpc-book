450b08a13d42e31a25efc61331c474e27671ac58
Leticia-Freire <leticiafcs4@gmail.com>
2021-02-24 15:49:16 -0300

    lazy Li Chao Tree - point query

---
+++ content/data-structures/lichao_lazy.h
@@ -0,0 +1,107 @@
+/**
+ * Author: LeticiaFCS
+ * Source: radoslav11, https://codeforces.com/blog/entry/86731
+ * Description: Lazy Li Chao Tree. Allows line add, segment add, segment update and point query.
+ * Status: Stress tested
+ * Time: $O(\log^2 N)$ for segment add, $O(\log N)$ for other operations
+ */
+
+template<typename T, T N>
+struct lichao_lazy{
+    static const T inf = -numeric_limits<T>::max() / 2;
+    bool first_best( T a, T b ){ return a > b; }
+    T get_best( T a, T b ){  return first_best(a, b) ? a : b;  }
+    struct line{ 
+        T m, b;
+        T operator()( T x ){ return m*x + b; } 
+        void apply(line other){
+            m += other.m;
+            b += other.b;
+        }
+    };
+    struct node{
+        line li, lazy;
+        node *left, *right;
+        node( line _li = {0, inf}): li(_li), lazy({0,0}), left(nullptr), right(nullptr){} 
+        void apply(line other){
+            li.apply(other);
+            lazy.apply(other);
+        }
+    };
+    node *root;
+    lichao_lazy( line li = {0, inf} ): root ( new node(li) ) {}
+    void propagateLazy(node *&cur){
+        if(cur == nullptr) return;
+        if(cur->left == nullptr) cur->left = new node;
+        if(cur->right == nullptr) cur->right = new node;
+        cur->left->apply( cur-> lazy);
+        cur->right->apply( cur-> lazy);
+        cur->lazy = {0, 0};
+    }
+    T query( T x , node *cur , T l, T r){
+        if(x < l || x > r || l > r) return inf;
+        if(cur == nullptr) return inf;
+        T mid = ( l + r ) / 2;
+        if(l != r) propagateLazy(cur);
+        T ans =  cur->li(x);
+        ans = get_best( ans , query(x, cur->left, l, mid) );
+        ans = get_best( ans , query(x, cur->right, mid+1, r) );
+        return ans;
+    }
+    T query( T x ){ return query( x, root, 0, N ); }
+    void add( line li, node *&cur, T l, T r){
+        if(cur == nullptr){ 
+            cur = new node(li);
+            return;
+        }
+        T mid = ( l + r ) / 2;
+        propagateLazy(cur);
+        if( first_best( li(mid), cur->li(mid) ) )
+            swap(li, cur->li);
+        if( first_best( li(l), cur->li(l) ) )
+            add(li, cur->left, l, mid);   
+        if( first_best( li(r), cur->li(r) ) )
+            add(li, cur->right, mid + 1, r);
+    }
+    void add( T m, T b ){ add( {m, b}, root, 0, N ); }
+    void propagateLine(node *&cur, T l, T r){
+        if(cur == nullptr) return;
+        T mid = ( l + r ) / 2;
+        add(cur->li, cur->left, l, mid);
+        add(cur->li, cur->right, mid+1, r);
+        cur->li = {0, inf};
+    }
+    void addSegment( line li, node *&cur, T l, T r, T lseg, T rseg){
+        if(r < lseg || l > rseg) return;
+        if(cur == nullptr) cur =  new node;
+        if(lseg <= l && r <= rseg){
+            add(li, cur, l, r);
+            return;
+        }
+        T mid = ( l + r ) / 2;
+        if(l != r){
+            propagateLazy(cur);
+            addSegment(li, cur->left, l, mid, lseg, rseg);
+            addSegment(li, cur->right, mid+1, r, lseg, rseg);
+        }
+    }
+    void addSegment( T m, T b, T left, T right){
+        addSegment( {m, b}, root, 0, N, left, right);
+    }
+    void updateSegment( line li, node *&cur, T l, T r, T lseg, T rseg){
+        if(r < lseg || l > rseg) return;
+        if(cur == nullptr) cur =  new node;
+        if(lseg <= l && r <= rseg){
+            cur->apply(li);
+            return;
+        }
+        T mid = ( l + r ) / 2;
+        propagateLazy(cur);
+        propagateLine(cur, l, r);
+        updateSegment(li, cur->left, l, mid, lseg, rseg);
+        updateSegment(li, cur->right, mid+1, r, lseg, rseg);
+    }
+    void updateSegment( T m, T b, T left, T right){
+        updateSegment( {m, b}, root, 0, N, left, right);
+    }
+};
---
+++ stress-tests/data-structures/lichao_lazy.cpp
@@ -0,0 +1,157 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#include "../../content/data-structures/lichao_lazy.h"
+
+using lint = int64_t;
+
+template<typename data_t, data_t sz>
+struct lichao_comp{
+    static const data_t inf = numeric_limits<data_t>::max() / 2;
+
+template<typename data_t1>
+    struct Line {
+        data_t1 a, b;
+        Line() : a(0), b(-inf) {}
+        Line(data_t1 a, data_t1 b) : a(a), b(b) {}
+
+        data_t1 get(data_t1 x) {
+            return a * x + b;
+        }
+
+        void add(Line x) {
+            a += x.a;
+            b += x.b;
+        }
+        };
+
+    struct Node {
+        Line<data_t> line = Line<data_t>();
+        Line<data_t> lazy = Line<data_t>(0, 0);
+
+        Node *lc = nullptr;
+        Node *rc = nullptr;
+
+        void apply(data_t l, data_t r, Line<data_t> v) {
+            line.add(v);
+            lazy.add(v);
+        }
+    };
+    Node *root;
+    lichao_comp():root(new Node){}
+
+    void PushLazy(Node* &n, data_t tl, data_t tr) {
+    if (n == nullptr) return;
+    if (n->lc == nullptr) n->lc = new Node();
+    if (n->rc == nullptr) n->rc = new Node();
+    data_t mid = (tl + tr) / 2;
+    n->lc->apply(tl, mid, n->lazy);
+    n->rc->apply(mid + 1, tr, n->lazy);
+    n->lazy = Line<data_t>(0, 0);
+    }
+
+    void PushLine(Node* &n, data_t tl, data_t tr) {
+    if (n == nullptr) return;
+    data_t mid = (tl + tr) / 2;
+    InsertLineKnowingly(n->lc, tl, mid, n->line);
+    InsertLineKnowingly(n->rc, mid + 1, tr, n->line);
+    n->line = Line<data_t>();
+    }
+
+    void InsertLineKnowingly(Node* &n, data_t tl, data_t tr, Line<data_t> x) {
+    if (n == nullptr) n = new Node();
+    if (n->line.get(tl) < x.get(tl)) swap(n->line, x);
+    if (n->line.get(tr) >= x.get(tr)) return;
+    if (tl == tr) return;
+    data_t mid = (tl + tr) / 2;
+    PushLazy(n, tl, tr);
+    if (n->line.get(mid) > x.get(mid)) {
+        InsertLineKnowingly(n->rc, mid + 1, tr, x);
+    } else {
+        swap(n->line, x);
+        InsertLineKnowingly(n->lc, tl, mid, x);
+    }
+    }
+
+    void InsertLine(Node* &n, data_t tl, data_t tr, data_t l, data_t r, Line<data_t> x) {
+    if (tr < l || r < tl || tl > tr || l > r) return;
+    if (n == nullptr) n = new Node();
+    if (l <= tl && tr <= r) return InsertLineKnowingly(n, tl, tr, x);
+    data_t mid = (tl + tr) / 2;
+    PushLazy(n, tl, tr);
+    InsertLine(n->lc, tl, mid, l, r, x);
+    InsertLine(n->rc, mid + 1, tr, l, r, x);
+    }
+
+    void AddLine(Node* &n, data_t tl, data_t tr, data_t l, data_t r, Line<data_t> x) {
+    if (tr < l || r < tl || tl > tr || l > r) return;
+    if (n == nullptr) n = new Node();
+    if (l <= tl && tr <= r) return n->apply(tl, tr, x);
+    data_t mid = (tl + tr) / 2;
+    PushLazy(n, tl, tr);
+    PushLine(n, tl, tr);
+    AddLine(n->lc, tl, mid, l, r, x);
+    AddLine(n->rc, mid + 1, tr, l, r, x);
+    }
+
+    data_t Query(Node* &n, data_t tl, data_t tr, data_t x) {
+    if (n == nullptr) return -inf;
+    if (tl == tr) return n->line.get(x);
+    data_t res = n->line.get(x);
+    data_t mid = (tl + tr) / 2;
+    PushLazy(n, tl, tr);
+    if (x <= mid) {
+        res = max(res, Query(n->lc, tl, mid, x));
+    } else {
+        res = max(res, Query(n->rc, mid + 1, tr, x));
+    }
+    return res;
+    }
+
+    void InsertLine(data_t l, data_t r, Line<data_t> x) {
+    return InsertLine(root, 0, sz - 1, l, r, x);
+    }
+
+    void AddLine(data_t l, data_t r, Line<data_t> x) {
+    return AddLine(root, 0, sz - 1, l, r, x);
+    }
+
+    data_t Query(data_t x) {
+        return Query(root, 0, sz - 1, x);
+    }
+};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    const lint sz = int(2e5);
+    lichao_comp<lint, sz+1> lichao1;
+    lichao_lazy<lint, sz> lichao2;
+
+    int q;
+    q = 300000;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(0, sz); 
+    while(q--){
+        int ty = q%3;
+        if(ty == 0){
+            lint m = distr(gen), b=distr(gen), l=distr(gen), r=distr(gen);
+            if(l > r) swap(l, r);
+            lichao1.InsertLine(l, r, {m,b});
+            lichao2.addSegment(m, b, l, r);
+        } else if(ty == 1){
+            lint m=distr(gen), b=distr(gen), l=distr(gen), r=distr(gen);
+            if(l > r) swap(l, r);
+            lichao1.AddLine(l, r, {m,b});
+            lichao2.updateSegment(m, b, l, r);
+        } else{
+            int x=distr(gen);
+            auto correct = lichao1.Query(x);
+            auto mine = lichao2.query(x);
+            assert( correct == mine);
+        }
+    }
+    
+    return 0;
+}
