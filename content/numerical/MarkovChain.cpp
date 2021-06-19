fa7f221292a54391d09c85cd6af7ff11b2d54e1c
LeticiaFCS <leticiafcs@dcc.ufrj.br>
2021-06-18 22:38:51 -0300

    Markov Chain

--- content/numerical/MarkovChain.cpp
+++ content/numerical/MarkovChain.cpp
@@ -1,44 +1,25 @@
 /**
  * Author: LeticiaFCS
- * Date: 2019-09-12
- * Description: Markov Chain 
+ * Date: 2021-06-18
+ * Description: Markov Chain
+ * Row sums of the transition matrix need to be 1 and prob vector sum also needs to be 1.
+ * Returns if the prob vector converges and the converged vector
  * Status: Working on URI Midnight Cowboy
+ * Not well tested! Arbitrary limits, be careful
  */
 
-//1-indexed
-int adj[N][N]; //adj matrix
-int out[N]; // out degree of the state
-double trans[N][N], prob[N];
-
-void create_prob(int n, int s=1){
-	for(int i=1;i<=n;i++) prob[i]=0;
-	prob[s]=1;
+template<typename D = double> 
+auto MarkovChain(vector<vector<D>> trans, vector<D> prob, D eps = 1e-8, int it = 40000){
+	int n = (int) trans.size();
+	vector<D> last;
+	bool converged = false;	
+	while( !converged && it-- ){
+		last.swap(prob);  prob = vector<D>(n);
+		converged = true;
+		for(int i = 0; i < n; i++) for(int j = 0; j < n; j++)
+			prob[i] += last[j] * trans[j][i];			
+		for(int i = 0; i < n && converged; i++)
+			converged = abs(last[i] - prob[i]) < eps;
+	}	
+	return make_pair(converged, prob);
 }
-void create_chain(int n){
-	for(int i=1;i<=n;i++){
-		if(out[i])
-			for(int j=1;j<=n;j++){
-				if(adj[i][j]) trans[i][j]=((double)adj[i][j])/out[i];
-				else trans[i][j]=0;
-			}
-		else
-			for(int j=1;j<=n;j++) trans[i][j]=1.0/n;
-	}
-}
-double proxprob[N];
-int aplica(int n){
-	for(int i=1;i<=n;i++) proxprob[i]=0;
-	for(int i=1;i<=n;i++)
-		for(int j=1;j<=n;j++)
-			proxprob[i]+=prob[j]*trans[j][i];
-	int dif=0;
-	for(int i=1;i<=n;i++){
-		dif+=abs(cmp_double(prob[i],proxprob[i]));
-		prob[i]=proxprob[i];
-	}
-	return dif;
-}
-void solve(int n){
-	while(aplica(n));
-}
-
