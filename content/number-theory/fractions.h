f9d650b6a448be99602e890c641326371b2b6774
Letícia Freire Carvalho de Sousa <leticiafcs4@gmail.com>
2021-08-19 17:37:59 -0300

    update fractions

--- content/number-theory/fractions.h
+++ content/number-theory/fractions.h
@@ -1,39 +1,42 @@
 /**
- * Author: BenQ
+ * Author: BenQ (adapted by LeticiaFCS)
  * Date: 
  * License: 
  * Source: 
  * Description: Template that helps deal with fractions.
  */
+template<typename num = long long>
 struct frac { /// start-hash
-    lint n, d;
-    frac() { n = 0, d = 1; }
-    frac(lint _n, lint _d) {
-        n = _n, d = _d;
-        lint g = __gcd(n,d); n /= g, d /= g;
+    num n, d;
+    frac() : n(0), d(1) { }
+    static num fgcd( const num &a, const num &b ){ return gcd(a, b); }
+    frac(num _n, num _d = 1): n(_n), d(_d){
+        num g = fgcd(n, d); n /= g, d /= g;
         if (d < 0) n *= -1, d *= -1;
+        assert(d != 0);
     }
-    frac(lint _n) : frac(_n,1) {}
     friend frac abs(frac F) { return frac(abs(F.n),F.d); }
-    friend bool operator<(const frac& l, const frac& r) { return l.n*r.d < r.n*l.d; }
+    friend bool operator<(const frac& l, const frac& r) { return l.n * r.d < r.n * l.d; }
     friend bool operator==(const frac& l, const frac& r) { return l.n == r.n && l.d == r.d; }
     friend bool operator!=(const frac& l, const frac& r) { return !(l == r); }
-    friend frac operator+(const frac& l, const frac& r) { return frac(l.n*r.d+r.n*l.d,l.d*r.d); }
-    friend frac operator-(const frac& l, const frac& r) { return frac(l.n*r.d-r.n*l.d,l.d*r.d); }
-    friend frac operator*(const frac& l, const frac& r) { return frac(l.n*r.n,l.d*r.d); }
-    friend frac operator*(const frac& l, int r) { return l*frac(r,1); } 
-    friend frac operator*(int r, const frac& l) { return l*r; } 
-    friend frac operator/(const frac& l, const frac& r) { return l*frac(r.d,r.n); }
-    friend frac operator/(const frac& l, const int& r) { return l/frac(r,1); }
-    friend frac operator/(const int& l, const frac& r) { return frac(l,1)/r; }
+    friend frac operator+(const frac& l, const frac& r) { 
+    	num g = fgcd(l.d, r.d);
+    	return frac( r.d / g * l.n + l.d / g * r.n, l.d / g * r.d);
+    }
+    friend frac operator-(const frac& l, const frac& r) { 
+    	num g = fgcd(l.d, r.d);
+    	return frac( r.d / g * l.n - l.d / g * r.n, l.d / g * r.d);
+    }
+    friend frac operator*(const frac& l, const frac& r) { return frac(l.n * r.n, l.d * r.d); }
+    friend frac operator/(const frac& l, const frac& r) { return l * frac(r.d, r.n); }
     friend frac& operator+=(frac& l, const frac& r) { return l = l+r; }
     friend frac& operator-=(frac& l, const frac& r) { return l = l-r; }
     template<class T> friend frac& operator*=(frac& l, const T& r) { return l = l*r; }
     template<class T> friend frac& operator/=(frac& l, const T& r) { return l = l/r; }
     friend ostream& operator<<(ostream& strm, const frac& a) {
-        strm << a.n;
-        if (a.d != 1) strm << "/" << a.d;
+        strm << a.n << "/" << a.d;
         return strm;
     }
 };
+
 
