9853ffd6c65678c0b4e8cae93db8916173e5b8f7
Letícia Freire Carvalho de Sousa <leticiafcs4@gmail.com>
2021-10-26 23:27:57 -0300

    block cut tree

--- content/graph/bcc.h
+++ content/graph/bcc.h
@@ -8,8 +8,11 @@
  *  are at least two distinct paths between any two nodes. Note that a node can
  *  be in several components. An edge which is not in a component is a bridge,
  *  i.e., not part of any cycle.
+ *  $make_bcc_tree$ constructs the block cut tree of given graph. The first
+ *  $comps.size()$ nodes represents the blocks, the others represents the
+ *  cut vertices.
  * Time: O(E + V)
- * Status: tested during MIPT ICPC Workshop 2017
+ * Status: tested during MIPT ICPC Workshop 2017, make_bcc_tree is not well tested
  * Usage:
  *  int eid = 0; ed.resize(N);
  *  for each edge (a,b) {
@@ -44,7 +47,40 @@ template<class F> int dfs(int at, int par, F& f) {
 }
 
 template<class F> void bicomps(F f) {
+	Time = 0;
+	st.resize(0);
 	num.assign(ed.size(), 0);
 	for(int i = 0; i < int(ed.size()); ++i) 
 		if (!num[i]) dfs(i, -1, f);
+}
+
+using vvi = vector<vector<int>>;
+tuple<vvi, vvi, vector<int>> make_bcc_tree(const G &ed, const vector<pair<int,int>> &edges){
+	int nart = 0, ncomp = 0, n = int(ed.size());
+	vector<int> inv(n);
+	vvi comps;
+	bicomps(ed, [&](const vector<int> &eid){
+		ncomp++;
+		set<int> cur;
+		for(int e: eid){			
+			cur.insert(edges[e].first);
+			cur.insert(edges[e].second);	
+		}
+		comps.push_back(vector<int>(cur.begin(), cur.end()));
+		for(int v: cur) inv[v]++;
+	} );
+	vector<int> art;
+	for(int u = 0; u < n; u++)
+		if(inv[u] > 1){
+			inv[u] = nart++;
+			art.push_back(u);
+		} else inv[u] = -1;
+	vvi tree(ncomp + nart);
+	for(int c = 0; c < ncomp; c++)
+		for(int u: comps[c])
+			if(inv[u] != -1){
+				tree[ c ].push_back( ncomp + inv[u] );
+				tree[ ncomp + inv[u] ].push_back( c );
+			}
+	return {tree, comps, art};
 }
