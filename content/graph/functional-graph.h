e2a179221a810469c135f22254c520f1c9b32952
Letícia Freire Carvalho de Sousa <leticiafcs4@gmail.com>
2021-10-29 00:02:59 -0300

    Update functional-graph.h

--- content/graph/functional-graph.h
+++ content/graph/functional-graph.h
@@ -1,92 +1,47 @@
 /**
- * Author: André Sousa
- * Description:  Called lumberjack technique, solve functional graphs 
- * problems for graphs, it's also pretty good for dp on trees.
- *  Consists in go cutting the leaves until there is no leaves,
- *  only cycles. For that we keep a processing queue of the leaves,
- *  note that during this processing time we go through all the childrens
- * of v before reaching a vertex v, therefore we can compute some
- * infos about the children, like subtree of a given vertex
- *
+ * Author: LeticiaFCS
+ * Description: finds the directions of the edges of given functional graph, returns pair of $parent$ and $indegree$ of each vertex.
+ * Useful together with functional-digraph.h.
  * Status: NOT tested
  */
-template<int T> struct Lumberjack {
-	int n, numcycle;
-	vector<int> subtree, order, par, cycle;
-	vector<int> parincycles, idxcycle, sz, st;
-	vector<int> depth, deg, cycles[T];
-	vector<bool> seen, incycle, leaf;
-	vector<vector<int>> graph;
-	void init(vector<vector<int>>& _graph, vector<int>& _deg){ 
-		init(_graph.size());
-		graph = _graph; deg = _deg; 
-	}
-	void init(int N) {
-		n = N;
-		order.resize(0);
-		subtree.assign(n, 0);
-		seen.assign(n, false);
-		sz = st = subtree;
-		parincycles = par = cycle = sz;
-		idxcycle = depth = deg = sz; 
-		incycle = leaf = seen;
-		vector<int> adj; graph.assign(n, adj);
-	}
-	int find_par(int u) {
-	    for (int v : graph[u]) if (!seen[v]) 
-	    	return v;
-	    return -1;
-	}
-	void find_cycle(int u){
-		int idx= ++numcycle, cur = 0, p = u;
-		st[idx] = u;
-		sz[idx] = 0;
-		cycles[idx].clear();
-		while (!seen[u]) {
-			seen[u] = incycle[u] = true;
-			par[u] = find_par(u);
-			if(par[u] == -1) par[u] = p;
-			parincycles[u] = u;
-			cycle[u] = idx;
-			idxcycle[u] = cur;
-			cycles[idx].push_back(u);
-			++sz[idx];
-			depth[u] = 0;
-			++subtree[u];
-			u = par[u];
-			++cur;
+pair<vector<int>, vector<int>> make_functional_digraph(const vector<vector<int>> &g, vector<int> deg){
+	int n = int(g.size());
+	vector<int> par(n), indeg(n);
+	vector<bool> vis(n);
+	queue<int> q;
+ 	for(int u=0; u<n; u++)
+		if(deg[u] == 1)
+			q.push(u);
+	while(!q.empty()){
+		int u = q.front();
+		q.pop();
+		vis[u] = true;
+		for(int v: g[u]){
+			if(vis[v]) continue;
+			par[u] = v;
+			indeg[v]++;
+			deg[v]--;
+			if(deg[v] == 1)
+				q.push(v);
 		}
 	}
-	void bfs() {
-		queue<int> q;
-		for (int i = 0; i < n; ++i)
-			if (deg[i] == 1){
-				seen[i] = leaf[i] = true;
-				q.push(i);
-			}
-		while(!q.empty()){
-			int v = q.front(); q.pop();
-			order.push_back(v);
-			++subtree[v];
-			int curpar = find_par(v);
-			deg[curpar]--;
-			subtree[curpar] += subtree[v];
-			if(deg[curpar] == 1){
-				q.push(curpar);
-				seen[curpar] = true;
-			}
-		}
-		numcycle = 0;
-		for (int i = 0; i < n; ++i)
-			if (!seen[i]) find_cycle(i);
-		for(int i = order.size()-1; i >= 0; --i){
-			int v = order[i], curpar = par[v];
-			parincycles[v] = parincycles[curpar];
-			cycle[v] = cycle[curpar];
-			incycle[v] = false;
-			idxcycle[v] = -1;
-			depth[v] = 1 + depth[curpar];
+	for(int u=0; u<n; u++){
+		if(vis[u]) continue;
+		int cur = u, nxt = -1;
+		while(nxt != u){
+			vis[cur] = true;
+			nxt = -1;
+			for(int x: g[u])
+				if(!vis[x]){
+					nxt = x;
+					break;
+				}	
+			if(nxt == -1)
+				nxt = u;
+			indeg[nxt]++;
+			par[cur] = nxt;
+			cur = nxt;
 		}
 	}
-};
-
+	return {par, indeg};	
+}
