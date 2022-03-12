1fd1542cc0eeb8ce8f83bea466cde7244752f3e2
ciafrino <chrisciafrino@ufrj.br>
2022-03-12 20:57:37 -0300

    Add implicit-treap.h

--- content/data-structures/implicit-treap.h
+++ content/data-structures/implicit-treap.h
@@ -12,17 +12,29 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 struct node {
     int v, p, sz;
     node *l, *r;
-    node(int k) : v(k), p(rng()), l(nullptr), r(nullptr), sz(0) {}
+    bool rev;
+    node(int k) : v(k), p(rng()), l(nullptr), rev(0), r(nullptr), sz(0) {}
 };
 int sz(node *t) {
     if (t == nullptr) return 0;
     return t->sz;
 }
+void push(node *t) {
+    if (t == nullptr) return;
+    if (t->rev) {
+        swap(t->l, t->r);
+        if (t->l != nullptr) t->l->rev ^= t->rev;
+        if (t->r != nullptr) t->r->rev ^= t->rev;
+        t->rev = 0;
+    }
+}
 void updsz(node *t) {
     if (t == nullptr) return;
+    push(t); push(t->l); push(t->r);
     t->sz = sz(t->l) + sz(t->r) + 1;
 }
 void split(node *t, node *&l, node *&r, int k) { //k on left
+    push(t);
     if (t == nullptr) l = r = nullptr;
     else if (k <= sz(t->l)) {
         split(t->l, l, t->l, k);
@@ -35,6 +47,7 @@ void split(node *t, node *&l, node *&r, int k) { //k on left
     updsz(t);
 }
 void merge(node *&t, node *l, node *r) {
+    push(l); push(r);
     if (l == nullptr) t = r;
     else if (r == nullptr) t = l;
     else if (l->p <= r->p) {
@@ -48,6 +61,7 @@ void merge(node *&t, node *l, node *r) {
     updsz(t);
 }
 void add(node *&t, node *c, int k) {
+    push(t);
     if (t == nullptr) t = c;
     else if (c->p >= t->p) {
         split(t, c->l, c->r, k);
@@ -58,6 +72,7 @@ void add(node *&t, node *c, int k) {
     updsz(t);
 }
 void del(node *&t, int k) {
+    push(t);
     if (t == nullptr) return;
     if (sz(t->l) == k) merge(t, t->l, t->r);
     else if (sz(t->l) > k) del(t->l, k);
