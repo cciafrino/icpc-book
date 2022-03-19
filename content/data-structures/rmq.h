97bb8e2f88bb6c8245d719735871a9dfd98807d9
LeticiaFCS <leticiafcs@dcc.ufrj.br>
2022-03-18 23:20:54 -0300

    rmq

--- content/data-structures/rmq.h
+++ content/data-structures/rmq.h
@@ -19,8 +19,7 @@ struct rmq_t : private Cmp {
     const T& min(const T& a, const T& b) const { return Cmp::operator()(a, b) ? a : b; }
     rmq_t() {}
     rmq_t(const vector<T>& values) : N(int(values.size())), table(__lg(N) + 1) {
-        table[0].resize(N);
-        for (int i = 0; i < N; ++i) table[0][i] = values[i];
+        table[0] = values;
         for (int a = 1; a < int(table.size()); ++a) {
             table[a].resize(N - (1 << a) + 1);
             for (int b = 0; b + (1 << a) <= N; ++b) 
--- stress-tests/data-structures/RMQ.cpp
+++ stress-tests/data-structures/RMQ.cpp
@@ -1,16 +1,16 @@
 #include "../utilities/template.h"
 
-#include "../../content/data-structures/RMQ.h"
+#include "../../content/data-structures/rmq.h"
 
 int main() {
 	srand(2);
 	rep(N,0,100) {
-		vi v(N);
+		vi v(N); 
 		rep(i,0,N) v[i] = i;
 		random_shuffle(all(v));
-		RMQ<int> rmq(v);
+		rmq_t<int> rmq(v);
 		rep(i,0,N) rep(j,i+1,N+1) {
-			int m = rmq.query(i,j);
+			int m = rmq.query(i,j-1);
 			int n = 1 << 29;
 			rep(k,i,j) n = min(n, v[k]);
 			assert(n == m);
