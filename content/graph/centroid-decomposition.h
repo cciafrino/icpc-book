9589cb5f8ce384ffabf8292e8055e93d73dc33cb
ciafrino <chrisciafrino@ufrj.br>
2022-03-29 20:24:11 -0300

    Refactor centroid-decomposition.h

--- content/graph/centroid-decomposition.h
+++ content/graph/centroid-decomposition.h
@@ -1,64 +1,81 @@
 /**
  * Author: Chris
  * Description: Divide and Conquer on Trees.
+ * Status: Tested on CSES2080
  * 
  */
-
-struct centroid_t {
-    int n;
-    vector<vector<int>> edges; 
+template<typename T> struct centroid_t {
+    int N;
+    vector<vector<int>> adj; 
     vector<vector<int>> dist; // dist to all ancestors
     vector<bool> blocked; // processed centroid
     vector<int> sz, depth, parent; // centroid parent
-    centroid_t(int _n) : n(_n), edges(n), blocked(n), sz(n), depth(n),
-        parent(n), dist(32 - __builtin_clz(n), vector<int>(n)) {}
-    void addEdge(int a, int b) {
-        edges[a].push_back(b);
-        edges[b].push_back(a);
+    centroid_t(int _n) : N(_n), adj(_n), dist(32 - __builtin_clz(_n), vector<int>(_n)), 
+    blocked(_n), sz(_n), depth(_n), parent(_n) {}
+    void add_edge(int a, int b) {
+        adj[a].push_back(b);
+        adj[b].push_back(a);
     }
-    void dfs_sz(int v, int p) { 
-        sz[v] = 1;
-        for (int u : edges[v]) {
-            if (u == p || blocked[u]) continue;
-            dfs_sz(u, v);
-            sz[v] += sz[u];
+    void dfs_sz(int cur, int prv) { 
+        sz[cur] = 1;
+        for (int nxt : adj[cur]) {
+            if (nxt == prv || blocked[nxt]) continue;
+            dfs_sz(nxt, cur);
+            sz[cur] += sz[nxt];
         }
     }
-
-    int find(int v, int p, int tsz) { // find a centroid
-        for (int u : edges[v]) 
-            if (!blocked[u] && u != p && 2*sz[u] > tsz) 
-                return find(u, v, tsz);
-        return v;
+    int find(int cur, int prv, int tsz) { 
+        for (int nxt : adj[cur]) 
+            if (!blocked[nxt] && nxt != prv && 2*sz[nxt] > tsz) 
+                return find(nxt, cur, tsz);
+        return cur;
     }
-
-    void dfs_dist(int v, int p, int layer, int d) {
-        dist[layer][v] = d;
-        for (int u : edges[v]) {
-            if (blocked[u] || u == p) continue;
-            dfs_dist(u, v, layer, d + 1);
+    void dfs_dist(int cur, int prv, int layer, int d) {
+        dist[layer][cur] = d;
+        for (int nxt : adj[cur]) {
+            if (blocked[nxt] || nxt == prv) continue;
+            dfs_dist(nxt, cur, layer, d + 1);
         }
     }
+    void get_path(int cur, int prv, int d, vector<int>& cur_path) {
+        cur_path.push_back(d);
+        for (int nxt : adj[cur]) {
+            if (nxt == prv || blocked[nxt]) continue;
+            get_path(nxt, cur, d + 1, cur_path);
+        }
+    } 
+    // solve for each subtree (cnt := # of paths of length K 
+    // that goes through vertex cur)
+    T solve_subtree(int cur, int prv, int K) {
+        vector<T> dp(sz[prv] + 1); dp[0] = 1;
+        T cnt = 0;
+        for (int nxt : adj[cur]) {
+            if (blocked[nxt]) continue;
+            vector<int> path;
+            get_path(nxt, cur, 1, path);
+            for (int d : path) {
+                if (d > K || K - d > sz[prv]) continue;
+                cnt += dp[K - d];
+            }
 
-    int solve(int v, int p) {
-        // solve the problem for each subtree here xD   
+            for (int d : path) dp[d] += 1;
+        }
+        return cnt;
     }
+    T decompose(int cur, int K, int layer = 0, int prv_root = -1) {
+        dfs_sz(cur, -1);
+        int root = find(cur, cur, sz[cur]);
+        blocked[root] = true;
+        depth[root] = layer;
+        parent[root] = prv_root;
+        dfs_dist(root, root, layer, 0);
 
-    int decompose(int v, int layer=0, int lst_x = -1) {
-        dfs_sz(v, -1);
-        int x = find(v, v, sz[v]);
-        blocked[x] = true;
-        depth[x] = layer;
-        parent[x] = lst_x;
-        dfs_dist(x, x, layer, 0);
-
-        int res = solve(x, v); // solving for each subtree
+        T res = solve_subtree(root, cur, K); 
         
-        for (int u : edges[x]) {
-            if (blocked[u]) continue;
-                res += decompose(u, layer + 1, x);
+        for (int nxt : adj[root]) {
+            if (blocked[nxt]) continue;
+            res += decompose(nxt, K, layer + 1, root);
         }
         return res;
     }
 };
-
