c545730624374fbec813876c4715b161654697c6
LeticiaFCS <leticiafcs@dcc.ufrj.br>
2022-03-29 18:45:44 -0300

    lichao

--- content/data-structures/lichao.h
+++ content/data-structures/lichao.h
@@ -5,17 +5,16 @@
  * Status: Tested on yosupo
  * Time: $O(\log^2 N)$ for segment add, $O(\log N)$ for other operations
  */
-
-template<typename T, T N>
+template<typename T, T L, T R>
 struct lichao_t{
     static const T inf = numeric_limits<T>::max() / 2;
     bool first_best( T a, T b ){ return a < b; }
     T get_best( T a, T b ){  return first_best(a, b) ? a : b;  }
-    struct line{ 
+    struct line{ // 785930
         T m, b;
         T operator()( T x ){ return m*x + b; } 
     };
-    struct node{
+    struct node{ // e92ef4
         line li;
         node *left, *right;
         node( line _li = {0, inf}): li(_li), left(nullptr), right(nullptr){}
@@ -27,22 +26,22 @@ struct lichao_t{
     node *root;
     lichao_t( line li = {0, inf} ): root ( new node(li) ) {}
     ~lichao_t(){ delete root; }
-    T query( T x , node *cur , T l, T r){ // c1cea
+    T query( T x , node *cur , T l, T r){ // e3e758
         if(cur == nullptr) return inf;
         if(x < l || x > r) return inf;
-        T mid = ( l + r ) / 2;   
+        T mid = ( l + r ) >> 1;   
         T ans =  cur->li(x);
         ans = get_best( ans , query(x, cur->left, l, mid) );
         ans = get_best( ans , query(x, cur->right, mid+1, r) );
         return ans;
     }
-    T query( T x ){ return query( x, root, 0, N ); }
-    void add( line li, node *&cur, T l, T r){ // 02eee
+    T query( T x ){ return query( x, root, L, R ); }
+    void add( line li, node *&cur, T l, T r){ // 0962ab
         if(cur == nullptr){ 
             cur = new node(li);
             return;
         }
-        T mid = ( l + r ) / 2;
+        T mid = ( l + r ) >> 1;
         if( first_best( li(mid), cur->li(mid) ) )
             swap(li, cur->li);
         if( first_best( li(l), cur->li(l) ) )
@@ -50,23 +49,22 @@ struct lichao_t{
         if( first_best( li(r), cur->li(r) ) )
             add(li, cur->right, mid + 1, r);
     }
-    void add( T m, T b ){ add( {m, b}, root, 0, N ); }
-    // 0a172
-    void addSegment( line li, node *&cur, T l, T r, T lseg, T rseg){
+    void add( T m, T b ){ add( {m, b}, root, L, R ); }
+    void addSegment( line li, node *&cur, T l, T r, T lseg, T rseg){ // d1fcf2
         if(r < lseg || l > rseg) return;
         if(cur == nullptr) cur =  new node;
         if(lseg <= l && r <= rseg){
             add(li, cur, l, r);
             return;
         }
-        T mid = ( l + r ) / 2;
+        T mid = ( l + r ) >> 1;
         if(l != r){
             addSegment(li, cur->left, l, mid, lseg, rseg);
             addSegment(li, cur->right, mid+1, r, lseg, rseg);
         }
     }
     void addSegment( T m, T b, T left, T right){
-        addSegment( {m, b}, root, 0, N, left, right);
+        addSegment( {m, b}, root, L, R, left, right);
     }
 };
 
