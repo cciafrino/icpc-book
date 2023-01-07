d8bade75f7c29ca0f51af05a94dd105a4e078c23
felipechen008 <felipechenwu@gmail.com>
2023-01-07 12:19:16 -0300

    changed aho

--- content/strings/aho-corasick.h
+++ content/strings/aho-corasick.h
@@ -1,24 +1,47 @@
 /**
- * Author: Chris
+ * Author: Chen
  * Source: 
  * Description:
  * Status: tested
  */
-vector<int> bfs; bfs.reserve(trie.size());
-vector<int> failure(trie.size());
-failure[0] = -1;
-top_failure[0] = -1;
-bfs.push_back(0);
-
-for (int z = 0; z < int(bfs.size()); ++z) {
-    int cur = bfs[z];
-    if (z) top_failure[cur] = is_end[failure[cur]] ? failure[cur] : top_failure[failure[cur]];
-    for (int c = 0; c < 26; ++c) {
+const int sigma = 26;
+array<int, sigma> init;
+for (int i = 0; i < sigma; i++) init[i] = -1;
+vector<array<int, sigma>> trie(1, init);
+vector<int> out(1, -1), parent(n, -1), ids(n);
+for (int i = 0; i < n; i++) {
+    int cur = 0;
+    for (char ch : s[i]) {
+        int c = ch - 'a';
         if (trie[cur][c] == -1) {
-            trie[cur][c] = z ? trie[failure[cur]][c] : 0;
-        } else {
-            failure[trie[cur][c]] = z ? trie[failure[cur]][c] : 0;
-            bfs.push_back(trie[cur][c]);
+            trie[cur][c] = (int)trie.size();
+            trie.push_back(init);
+            out.push_back(-1);
+        }
+        cur = trie[cur][c];
+    }
+    if (out[cur] == -1) out[cur] = i;
+    ids[i] = out[cur];   
+}
+vector<int> bfs; bfs.reserve(trie.size());
+vector<int> f(trie.size());
+for (int c = 0; c < sigma; c++) {
+    if (trie[0][c] == -1) trie[0][c] = 0;
+    else bfs.push_back(trie[0][c]);
+}
+for (int z = 0; z < (int)bfs.size() ; z++) {
+    int cur = bfs[z];
+    for (int c = 0; c < sigma; c++) {
+        if (trie[cur][c] == -1) {
+            trie[cur][c] = trie[f[cur]][c];
+        }
+        else {
+            int nxt = trie[cur][c];
+            int fail = trie[f[cur]][c];
+            if (out[nxt] == -1) out[nxt] = out[fail];
+            else parent[out[nxt]] = out[fail];
+            f[nxt] = fail;
+            bfs.push_back(nxt);
         }
     }
 }
