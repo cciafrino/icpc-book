73bab9624a29d2ef36fed45309f610e06181820b
leticiafcs <leticiafcs4@gmail.com>
2023-03-05 15:32:11 -0300

    typo primitive roots

--- content/number-theory/primitive-roots.h
+++ content/number-theory/primitive-roots.h
@@ -4,7 +4,7 @@
  * License: 
  * Source: 
  * Description: $a$ is a primitive root mod $n$ if for every number $x$ coprime to $n$
- * there is an integer $z$ s.t. $x \equiv g^z  \pmod{n}$. The number of primitive roots mod n, if
+ * there is an integer $z$ s.t. $x \equiv a^z  \pmod{n}$. The number of primitive roots mod n, if
  * there are any, is equal to $phi(phi(N))$. If $m$ isnt prime, replace $m-1$ by $phi(m)$.
  * Status: 
  * Time: $O(log(N))$
