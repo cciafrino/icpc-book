2fc0f03e002932da6950004ca545ebc3f14c5d6d
Chris Ciafrino <cciafrino@gmail.com>
2023-06-19 17:33:04 -0300

    Shortening suffix-array a bit

--- content/strings/suffix-array.h
+++ content/strings/suffix-array.h
@@ -7,25 +7,22 @@
  * Time: $O(N \log N)$ where $N$ is the length of the string
  * for creation of the SA. $O(N)$ for longest common prefixes.
  */
-#include<../data-structures/RMQ.h>
+#include<../data-structures/rmq.h>
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 struct suffix_array_t { 
-    int N, H; 
-    vector<int> sa, invsa;
-    vector<int> lcp;
-    rmq_t<pair<int, int>> RMQ;
+    int N, H; vector<int> sa, invsa, lcp;
+    rmq_t<pair<int, int>> rmq;
     bool cmp(int a, int b) { return invsa[a + H] < invsa[b + H]; }
     void ternary_sort(int a, int b) { // 262db8
         if (a == b) return;
-        int pivot = sa[a + rng() % (b - a)];
-        int left = a, right = b;
-        for (int i = a; i < b; ++i) if (cmp(sa[i], pivot)) swap(sa[i], sa[left++]);
-        for (int i = b-1; i >= left; --i) if (cmp(pivot, sa[i])) swap(sa[i], sa[--right]);
-        ternary_sort(a, left);
-        for (int i = left; i < right; ++i) invsa[sa[i]] = right-1;
-        if (right-left == 1) sa[left] = -1;
-        ternary_sort(right, b);
+        int md = sa[a+rng() % (b-a)], lo = a, hi = b;
+        for (int i = a; i < b; ++i) if (cmp(sa[i], md)) swap(sa[i], sa[lo++]);
+        for (int i = b-1; i >= lo; --i) if (cmp(md, sa[i])) swap(sa[i], sa[--hi]);
+        ternary_sort(a, lo);
+        for (int i = lo; i < hi; ++i) invsa[sa[i]] = hi-1;
+        if (hi-lo == 1) sa[lo] = -1;
+        ternary_sort(hi, b);
     } 
     suffix_array_t() {} 
     template<typename I>
@@ -42,17 +39,17 @@ struct suffix_array_t {
                 else { j = invsa[sa[i]] + 1; ternary_sort(i, j); } 
         for (int i = 0; i < N; ++i) sa[invsa[i]] = i;
 
-        lcp.resize(N); int res = 0;
+        lcp.resize(N); int K = 0;
         for (int i = 0; i < N; ++i) {
-            if (invsa[i] > 0) while (v[i + res] == v[sa[invsa[i] - 1] + res]) ++res;
-            lcp[invsa[i]] = res; res = max(res - 1, 0);
+            if (invsa[i] > 0) while (v[i + K] == v[sa[invsa[i] - 1] + K]) ++K;
+            lcp[invsa[i]] = K; K = max(K - 1, 0);
         }
         vector<pair<int, int>> lcp_index(N);
         for (int i = 0; i < N; ++i) lcp_index[i] = {lcp[i], 1 + i};
-        RMQ = rmq_t<pair<int, int>>(move(lcp_index));
+        rmq = rmq_t<pair<int, int>>(std::move(lcp_index));
     } 
-    pair<int, int> rmq_query(int a, int b) const { return RMQ.query(a, b); } 
-    pair<int, int> get_split(int a, int b) const { return RMQ.query(a, b-1); }
+    pair<int, int> rmq_query(int a, int b) const { return rmq.query(a, b); } 
+    pair<int, int> get_split(int a, int b) const { return rmq.query(a, b-1); }
     int get_lcp(int a, int b) const { // cde9bf
         if (a == b) return N - a;
         a = invsa[a], b = invsa[b];
