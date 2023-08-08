f16d2989916526a579b53a328bc8761caa2e5dd0
Chris Ciafrino <cciafrino@gmail.com>
2023-08-08 20:09:03 -0300

    Shortening modular-arithmetic.h and mod-inv.h

--- content/number-theory/mod-inv.h
+++ content/number-theory/mod-inv.h
@@ -3,24 +3,7 @@
  * Description: Find $x$ such that $ax \equiv 1$(mod $m$). The inverse
  * only exist if $a$ and $m$ are coprimes.
  */
-
-template<typename T>
-T modinv(T a) { // a^-1 (mod 2^64)
-	assert(a & 1);
-	T b = ((a << 1) + a) * ((a << 1) + a);
-	b *= 2 - a * b;
-	b *= 2 - a * b;
-	b *= 2 - a * b;
-	b *= 2 - a * b;
-	return b;
-}
-template<typename T>
-T modinv(T a, T m) { // a^-1 (mod m)
-	assert(m > 0);
-	if (m == 1) return 0;
-	a %= m;
-	if (a < 0) a += m;
-	assert(a != 0);
-	if (a == 1) return 1;
-	return m - modinv(m, a) * m/a;
+int minv(int a, int m) {
+	a %= m; assert(a);
+	return a == 1 ? 1 : int(m - int64_t(minv(m, a)) * m / a);
 }
--- content/number-theory/modular-arithmetic.h
+++ content/number-theory/modular-arithmetic.h
@@ -7,57 +7,37 @@
  * Time:
  * Status: extensively used.
  */
+#include"mod-inv.h"
 template<unsigned M_> struct modnum {
 	static constexpr unsigned M = M_;
-	using ll = long long; using ull = unsigned long long; unsigned x;
-
-	// ec131c
-	constexpr modnum() : x(0U) {}
-	constexpr modnum(unsigned a) : x(a % M) {}
-	constexpr modnum(ull a) : x(a % M) {}
-	constexpr modnum(int a) : x(((a %= static_cast<int>(M)) < 0) ? (a + static_cast<int>(M)) : a) {}
-	constexpr modnum(ll a) : x(((a %= static_cast<ll>(M)) < 0) ? (a + static_cast<ll>(M)) : a) {}
-	// da738b
+	using ll = int64_t; using ull = uint64_t; unsigned x;
+	modnum& norm(unsigned a) { x = a < M ? a : a - M; return *this; }
+	constexpr modnum(ll a = 0U) : x(unsigned((a %= ll(M)) < 0 ? a + ll(M) : a)) {}
 	explicit operator int() const { return x; }
-	modnum& operator+=(const modnum& a) { x = ((x += a.x) >= M) ? (x - M) : x; return *this; }
-	modnum& operator-=(const modnum& a) { x = ((x -= a.x) >= M) ? (x + M) : x; return *this; }
-	modnum& operator*=(const modnum& a) { x = unsigned((static_cast<ull>(x) * a.x) % M); return *this; }
+	modnum& operator+=(const modnum& a) { return norm(x + a.x); }
+	modnum& operator-=(const modnum& a) { return norm(x - a.x + M); }
+	modnum& operator*=(const modnum& a) { x = unsigned(ull(x) * a.x % M); return *this; }
 	modnum& operator/=(const modnum& a) { return (*this *= a.inv()); }
-	// ef603f
 	modnum operator+(const modnum& a) const { return (modnum(*this) += a); }
 	modnum operator-(const modnum& a) const { return (modnum(*this) -= a); }
 	modnum operator*(const modnum& a) const { return (modnum(*this) *= a); }
 	modnum operator/(const modnum& a) const { return (modnum(*this) /= a); }
-	modnum operator+() const { return *this; }
-	modnum operator-() const { modnum a; a.x = x ? (M - x) : 0U; return a; }
-	// b8b96c
 	template<typename T> friend modnum operator+(T a, const modnum& b) { return (modnum(a) += b); }
 	template<typename T> friend modnum operator-(T a, const modnum& b) { return (modnum(a) -= b); }
 	template<typename T> friend modnum operator*(T a, const modnum& b) { return (modnum(a) *= b); }
 	template<typename T> friend modnum operator/(T a, const modnum& b) { return (modnum(a) /= b); }
-
+	modnum operator+() const { return *this; }
+	modnum operator-() const { return modnum() - *this; }
 	modnum pow(ll e) const {
-		if (e < 0) return inv().pow(-e);
-		modnum x2 = x, xe = 1U;
-		for (; e; e >>= 1) {
-			if (e & 1) xe *= x2;
-			x2 *= x2;
-		}
+		modnum b = x, xe = 1U;
+		for (; e; e >>= 1) { if (e & 1) xe *= b; b *= b; }
 		return xe;
 	}
-	modnum inv() const {
-		unsigned a = x, b = M; int y = 1, z = 0;
-		while (a) {
-			const unsigned q = (b/a), c = (b - q*a); 
-			b = a, a = c; const int w = z - static_cast<int>(q) * y;
-			z = y, y = w;
-		} assert(b == 1U); return modnum(z);
-	}
+	modnum inv() const { return minv(x, M); }
 	friend modnum inv(const modnum& a) { return a.inv(); }
-
 	explicit operator bool() const { return x; }
 	friend bool operator==(const modnum& a, const modnum& b) { return a.x == b.x; }
 	friend bool operator!=(const modnum& a, const modnum& b) { return a.x != b.x; }
 	friend ostream &operator<<(ostream& os, const modnum& a) { return os << a.x; }
-	friend istream &operator>>(istream& in, modnum& n) { ull v_; in >> v_; n = modnum(v_); return in; }
+	friend istream &operator>>(istream& in, modnum& n) { ll v_; in >> v_; n = modnum(v_); return in; }
 };
