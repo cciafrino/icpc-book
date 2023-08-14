f88123b2752a88a29b9432ba965ca51fcf00d882
Chris Ciafrino <cciafrino@gmail.com>
2023-08-14 00:05:38 -0300

    Shortens lagrange.h

--- content/numerical/sum-of-powers.h
+++ content/numerical/sum-of-powers.h
@@ -9,7 +9,6 @@
  */
 #include "../number-theory/modular-arithmetic.h"
 #include "/lagrange.h"
-
 vector<num> get_monomials(int N, long long d) {
 	vector<int> pfac(N);
 	for (int i = 2; i < N; ++i) pfac[i] = i;
@@ -23,9 +22,8 @@ vector<num> get_monomials(int N, long long d) {
 	}
 	return pw;
 }
-
 num sum_of_power_limit(num r, int d, const vector<num>& fs) {
-	Combinatorics<num> M(d + 2);
+	interpolator_t<num> M(d + 2);
 	vector<num> qs(d + 1); qs[0] = 1;
 	for (int x = 1; x <= d; ++x) qs[x] = qs[x - 1] * r;
 	num ans = 0, cur_sum = 0;
@@ -39,20 +37,17 @@ num sum_of_power_limit(num r, int d, const vector<num>& fs) {
 	ans *= (1 - r).pow(-(d + 1)) * fac[d + 1];
 	return ans;
 }
-
 num sum_of_power(num r, int d, const vector<num>& fs, long long N) {
 	if (r == 0) return (0 < N) ? fs[0] : 0;
-	Combinatorics<num> M(d + 10);
-	vector<num> gs(d + 2); gs[0] = 0;
-	num rr = 1;
+	interpolator_t<num> M(d + 10);
+	vector<num> gs(d + 2); gs[0] = 0; num rr = 1;
 	for (int x = 0; x <= d; ++x) {
 		gs[x + 1] = gs[x] + rr * fs[x];
 		rr *= r;
 	}
 	if (r == 1) return M.interpolate(gs, N);
 	const num c = sum_of_power_limit(r, d, fs);
-	const num r_inv = r.inv();
-	num rr_inv = 1;
+	const num r_inv = r.inv(); num rr_inv = 1;
 	for (int x = 0; x <= d + 1; ++x) {
 		gs[x] = rr_inv * (gs[x] - c);
 		rr_inv *= r_inv;
