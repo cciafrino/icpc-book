0db7924c9e710b135aa253028165542bc63f0265
Chris Ciafrino <cciafrino@gmail.com>
2023-08-17 23:19:53 -0300

    Change linear-recurrence to use bostan-mori instead of kitamasa

--- content/numerical/linear-recurrence.h
+++ content/numerical/linear-recurrence.h
@@ -1,38 +1,33 @@
 /**
- * Author: Lucian Bicsi / Andrew He
- * Date: 2018-02-14
+ * Author: Chris
+ * Date: 2023
  * License: CC0
- * Source: Chinese material
- * Description: Generates the $k$'th term of an $n$-order
+ * Source: https://codeforces.com/blog/entry/96199?#comment-852652
+ * Description: Bostan-Mori algorithm. Generates the $k$'th term of an $n$-order
  * linear recurrence $S[i] = \sum_j S[i-j-1]tr[j]$,
  * given $S[0 \dots n-1]$ and $tr[0 \dots n-1]$.
  * Faster than matrix multiplication.
  * Useful together with Berlekamp--Massey.
- * Usage: linearRec({0, 1}, {1, 1}, k) // k'th Fibonacci number
- * Time: O(n^2 \log k)
+ * Usage: linear_rec({0, 1}, {1, 1}, k) // k'th Fibonacci number
+ * Time: O(n \log n \log k)
  * Status: bruteforce-tested mod 5 for n <= 5
  */
-#include "ModularArithmetic.h"
-
-template <typename num>
-num linearRec(const vector<num>& S, const vector<num>& tr, lint k) {
-	int n = int(tr.size());
-	assert(S.size() >= tr.size());
-	auto combine = [&](vector<num> a, vector<num> b) {
-		vector<num> res(n * 2 + 1);
-		for (int i = 0; i <= n; i++) for (int j = 0; j <= n; j++) res[i + j] += a[i] * b[j];
-		for (int i = 2 * n; i > n; --i) for (int j = 0; j < n; j++)
-			res[i - 1 - j] += res[i] * tr[j];
-		res.resize(n + 1);
-		return res;
-	};
-	vector<num> pol(n + 1), e(pol);
-	pol[0] = e[1] = 1;
-	for (++k; k; k /= 2) {
-		if (k % 2) pol = combine(pol, e);
-		e = combine(e, e);
+#include "../modular-arithmetic.h"
+template<typename T> 
+T linear_rec(const vector<T>& S, const vector<T>& tr, ll K) {
+	const int N = int(tr.size());
+	vector<T> qs(N + 1); qs[0] = 1;
+	for (int i = 0; i < N; ++i) qs[i + 1] = -tr[i];
+	auto fs = fft.convolve(S, qs); fs.resize(N);
+	for (; K; K /= 2) {
+		auto qneg = qs;
+		for (int i = 1; i <= N; i += 2) qneg[i] = -qneg[i];
+		fs = fft.convolve(fs, qneg), qs = fft.convolve(qs, qneg);
+		for (int i = 0; i < N; ++i) {
+			fs[i] = fs[2 * i + (K & 1)];
+			qs[i] = qs[2 * i];
+		}
+		qs[N] = qs[2*N]; fs.resize(N), qs.resize(N+1);
 	}
-	num res = 0;
-	for (int i = 0; i < n; i++) res += pol[i + 1] * S[i];
-	return res;
+	return fs[0];
 }
