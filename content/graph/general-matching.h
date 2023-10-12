e9bb7f6b3fbf03219835bba63cdddce1371d3671
Chris Ciafrino <cciafrino@gmail.com>
2023-10-12 13:06:00 -0300

    Update general-matching.h

--- content/graph/general-matching.h
+++ content/graph/general-matching.h
@@ -1,67 +1,54 @@
 /**
- * Author: Koosaga
+ * Author: Yui Hosaka
  * Description: Maximum Matching for general graphs 
  * (undirected and non bipartite) using Edmond's Blossom Algorithm.
  * Time: $O(EV^2)$ 
+ * Status: tested on yosupo
  */
 struct blossom_t {
-	int t, n; // 1-based indexing!!
-	vector<vector<int>> edges;
-	vector<int> seen, parent, og, match, aux, Q;
-	blossom_t(int _n) : n(_n), edges(n+1), seen(n+1), 
-	parent(n+1), og(n+1), match(n+1), aux(n+10), t(0) {}
-	void addEdge(int u, int v) {
-		edges[u].push_back(v);
-		edges[v].push_back(u);
+	int N, M; vector<vector<int>> adj;
+	vector<int> match, ts, ps; vector<array<int, 2>> fs;
+	blossom_t(auto& G) : N(int(G.size())), M(0), adj(G), match(N,-1), ts(N,-1), ps(N,-1), fs(N,{-1,-1}) {}
+	int root(int a) {
+		return (ts[a] != M || !~ps[a]) ? a : (ps[a] = root(ps[a]));
 	}
-	void augment(int u, int v) {
-		int pv = v, nv; // flip states of edges on u-v path
-		do {
-			pv = parent[v]; nv = match[pv];
-			match[v] = pv; match[pv] = v; v = nv;
-		} while(u != pv);
-	}
-	int lca(int v, int w) { // find LCA in O(dist)
-		++t;
-		while (1) {
-			if (v) {
-				if (aux[v] == t) return v; aux[v] = t;
-				v = og[parent[match[v]]];
-			} swap(v, w);
+	void rematch(int a, int b) {
+		const int w = match[a]; match[a] = b; auto [x, y] = fs[a];
+		if (~w && match[w] == a) {
+			if (~y) rematch(x, y), rematch(y, x);
+			else match[w] = x, rematch(x, w);
 		}
 	}
-	void blossom(int v, int w, int a) {
-		while (og[v] != a) {
-			parent[v] = w; w = match[v]; // go other way around cycle
-			if(seen[w] == 1) Q.push_back(w), seen[w] = 0;
-			og[v] = og[w] = a;      // merge into supernode
-			v = parent[w];
-		}
-	}
-	bool bfs(int u) {
-		for (int i = 1; i <= n; ++i) seen[i] = -1, og[i] = i;
-		Q = vector<int>(); Q.push_back(u); seen[u] = 0;
-		for(int i = 0; i < Q.size(); ++i) {
-			int v = Q[i]; 
-			for(auto &x : edges[v]) if (seen[x] == -1) {
-				parent[x] = v; seen[x] = 1;
-				if (!match[x]) return augment(u, x), true;
-				Q.push_back(match[x]); seen[match[x]] = 0;
-			} else if (seen[x] == 0 && og[v] != og[x]) {
-				int a = lca(og[v], og[x]);
-				blossom(x, v, a); blossom(v, x, a);
+	bool augment(int src) {
+		vector<int> bfs = {src}; bfs.reserve(N);
+		ts[src] = M; ps[src] = -1; fs[src] = {-1,-1};
+		for (int z = 0; z < int(bfs.size()); ++z) {
+			int cur = bfs[z];
+			for (int nxt : adj[cur]) if (nxt != src) {
+				if (match[nxt] == -1) {
+					match[nxt] = cur; rematch(cur, nxt); return true;
+				}
+				if (ts[nxt] == M) {
+					int a = root(cur), b = root(nxt), m = src;
+					if (a == b) continue;
+					while (a != src || b != src) {
+						if (b != src) swap(a, b);
+						if (fs[a][0]==cur&&fs[a][1]==nxt) { m = a; break; }
+						fs[a] = {cur, nxt}; a = root(fs[match[a]][0]);
+					}
+					for (const int r : {root(cur), root(nxt)})
+						for (int v = r; v != m; v = root(fs[match[v]][0]))
+							ts[v] = M, ps[v] = m, bfs.push_back(v);
+				} else if (ts[match[nxt]] != M) {
+					fs[nxt] = {-1, -1}; ts[match[nxt]] = M;
+					ps[match[nxt]] = nxt; fs[match[nxt]] = {cur, -1};
+					bfs.push_back(match[nxt]);
+				}
 			}
 		} return false;
 	}
-	int solve() {
-		int ans = 0; // find random matching (not necessary,
-		vector<int> V(n-1); iota(V.begin(), V.end(), 1);
-		shuffle(V.begin(), V.end(), mt19937(0x94949));
-		for(auto &x : V) if(!match[x]) 
-			for(auto &y : edges[x]) if (!match[y]) {
-				match[x] = y, match[y] = x; ++ans; break;
-			}
-		for(int i = 1; i <= n; ++i) if (!match[i] && bfs(i)) ++ans;
-		return ans;
+	int run() {
+		for(int v = 0; v < N; ++v) if(!~match[v]) M += augment(v);
+		return M;
 	}
 };
