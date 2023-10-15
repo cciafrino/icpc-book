5cc263ab49291ee915eff88cd31bdeb9d013f7c8
Chris Ciafrino <cciafrino@gmail.com>
2023-10-15 20:52:34 -0300

    Updating implicit-treap.h

--- content/data-structures/implicit-treap.h
+++ content/data-structures/implicit-treap.h
@@ -7,100 +7,79 @@
  *  is easy to augment with additional data.
  * Time: $O(\log N)$
  */
-
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 struct node {
-	int v, p, sz;
-	node *l, *r;
-	bool rev;
-	node(int k) : v(k), p(rng()), l(nullptr), rev(0), r(nullptr), sz(0) {}
+	int val, p, sz; bool rev;
+	array<node*, 2> c{nullptr, nullptr};
+	node(int k) : val(k), p(rng()), sz(0), rev(false) {}
+	~node() {
+		delete c[0];
+		delete c[1];
+	}
 };
-int sz(node *t) {
-	if (t == nullptr) return 0;
-	return t->sz;
+inline int sz(node *t) {
+	return (!t ? 0 : t->sz);
 }
-void push(node *t) {
-	if (t == nullptr) return;
+inline void push(node *t) {
+	if (!t) return;
 	if (t->rev) {
-		swap(t->l, t->r);
-		if (t->l != nullptr) t->l->rev ^= t->rev;
-		if (t->r != nullptr) t->r->rev ^= t->rev;
+		swap(t->c[0], t->c[1]);
+		if (t->c[0]) t->c[0]->rev ^= t->rev;
+		if (t->c[1]) t->c[1]->rev ^= t->rev;
 		t->rev = 0;
 	}
 }
-void updsz(node *t) {
-	if (t == nullptr) return;
-	push(t); push(t->l); push(t->r);
-	t->sz = sz(t->l) + sz(t->r) + 1;
+inline void pull(node *t) {
+	if (!t) return;
+	push(t); push(t->c[0]); push(t->c[1]);
+	t->sz = sz(t->c[0]) + sz(t->c[1]) + 1;
 }
-void split(node *t, node *&l, node *&r, int k) { //k on left
+inline void split(node *t, node *&a, node *&b, int k) { //k on left
 	push(t);
-	if (t == nullptr) l = r = nullptr;
-	else if (k <= sz(t->l)) {
-		split(t->l, l, t->l, k);
-		r = t;
-	} 
-	else {
-		split(t->r, t->r, r, k-1-sz(t->l));
-		l = t;
+	if (!t) a = b = nullptr;
+	else if (k <= sz(t->c[0])) {
+		split(t->c[0], a, t->c[0], k);
+		b = t;
+	} else {
+		split(t->c[1], t->c[1], b, k-1-sz(t->c[0]));
+		a = t;
 	}
-	updsz(t);
+	pull(t);
 }
-void merge(node *&t, node *l, node *r) {
-	push(l); push(r);
-	if (l == nullptr) t = r;
-	else if (r == nullptr) t = l;
-	else if (l->p <= r->p) {
-		merge(l->r, l->r, r);
-		t = l;
+inline void merge(node *&t, node *a, node *b) {
+	push(a); push(b);
+	if (!a) t = b;
+	else if (!b) t = a;
+	else if (a->p <= b->p) {
+		merge(a->c[1], a->c[1], b);
+		t = a;
+	} else {
+		merge(b->c[0], a, b->c[0]);
+		t = b;
 	}
-	else {
-		merge(r->l, l, r->l);
-		t = r;
-	}
-	updsz(t);
+	pull(t);
 }
-void add(node *&t, node *c, int k) {
+inline void add(node *&t, node *a, int k) {
 	push(t);
-	if (t == nullptr) t = c;
-	else if (c->p >= t->p) {
-		split(t, c->l, c->r, k);
-		t = c;
-	}
-	else if (sz(t->l) >= k) add(t->l, c, k);
-	else add(t->r, c, k-1-sz(t->l));
-	updsz(t);
+	if (!t) t = a;
+	else if (a->p >= t->p) {
+		split(t, a->c[0], a->c[1], k);
+		t = a;
+	} else if (sz(t->c[0]) >= k) add(t->c[0], a, k);
+	else add(t->c[1], a, k-1-sz(t->c[0]));
+	pull(t);
 }
 void del(node *&t, int k) {
 	push(t);
-	if (t == nullptr) return;
-	if (sz(t->l) == k) merge(t, t->l, t->r);
-	else if (sz(t->l) > k) del(t->l, k);
-	else del(t->r, k);
-	updsz(t);
+	if (!t) return;
+	if (sz(t->c[0]) == k) merge(t, t->c[0], t->c[1]);
+	else if (sz(t->c[0]) > k) del(t->c[0], k);
+	else del(t->c[1], k);
+	pull(t);
 }
-void print(node *t) {
-	if (r == nullptr) return;
-	print(t->l);
-	cout << t->v << ' ';
-	print(t->r); 
-}
-
-int main() {
-	node *treap = nullptr;
-	while(1) {
-		int a;
-		cin >> a;
-		if (a == 1) {
-			int c, d;
-			cin >> c >> d;
-			node *r = new node(d);
-			add(treap, r, c);
-		} else if (a == 2) {
-			int d;
-			cin >> d;
-			del(treap, d);
-		} 
-		print(treap);
-	}
+inline void dump_treap(node *t) {
+	if (!t) return;
+	push(t);
+	dump_treap(t->c[0]);
+	cerr << t->val << ' ';
+	dump_treap(t->c[1]);
 }
