6ef49a47d9e520342818991f5d80467e8531247f
LeticiaFCS <leticiafcs4@gmail.com>
2023-10-15 21:49:47 -0300

    line container lint --> ll

--- content/data-structures/line-container.h
+++ content/data-structures/line-container.h
@@ -9,13 +9,13 @@
  * Status: stress-tested
  */
 struct Line {
-	mutable lint k, m, p;
+	mutable ll k, m, p;
 	bool operator<(const Line& o) const { return k < o.k; }
-	bool operator<(lint x) const { return p < x; }
+	bool operator<(ll x) const { return p < x; }
 };
 struct LineContainer : multiset<Line, less<>> {
-	static const lint inf = LLONG_MAX; //for doubles 1/.0
-	lint div(lint a, lint b) { //for doubles a/b
+	static const ll inf = LLONG_MAX; //for doubles 1/.0
+	ll div(ll a, ll b) { //for doubles a/b
 		return a / b - ((a ^ b) < 0 && a % b); }
 	bool isect(iterator x, iterator y) {
 		if (y == end()) { x->p = inf; return false; }
@@ -23,14 +23,14 @@ struct LineContainer : multiset<Line, less<>> {
 		else x->p = div(y->m - x->m, x->k - y->k);
 		return x->p >= y->p;
 	}
-	void add(lint k, lint m) {
+	void add(ll k, ll m) {
 		auto z = insert({k, m, 0}), y = z++, x = y;
 		while (isect(y, z)) z = erase(z);
 		if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
 		while ((y = x) != begin() && (--x)->p >= y->p) 
 			isect(x, erase(y));
 	}
-	lint query(lint x) {
+	ll query(ll x) {
 		assert(!empty()); auto l = *lower_bound(x);
 		return l.k * x + l.m;
 	}
