4266c92be0c9987eb76bd4db04aaad1df782ca19
Chris Ciafrino <cciafrino@gmail.com>
2024-04-02 20:51:00 -0300

    Add persistent segtree and sparse/2d

--- content/data-structures/persistent-segtree.h
+++ content/data-structures/persistent-segtree.h
@@ -1,37 +1,50 @@
 /**
- * Author: Chris
- * Source: Zhou 
- * Description: Persistent implementation of a segment tree. This one compute the kth smallest element in a subarray $[a, b]$. 
- * Time: 
+ * Author: Antoni Dlugosz
+ * Source:
+ * Description: Sparse ($N$ can be up to 1e18) persistent segment tree.
+ * Time:
+ * Status: Tested on atcoder, dmoj and yosupo library checker.
  */ 
-struct segtree_t {
-	struct snapshot {
-		int cnt, linkl, linkr;
-		snapshot() : cnt(0), linkl(0), linkr(0) {}
-		snapshot(int _cnt, int l, int r) : cnt(_cnt), linkl(l), linkr(r) {}
+template<typename T> struct seg_tree {
+	T idnt = 0; // identity value
+	T f(T l, T r) { return l + r; }
+	struct node {
+		int l = 0, r = 0;
+		T x;
+		node(T _x) : x(_x) {}
 	};
-	int id;
-	vector<snapshot> tree;
-	segtree_t() {}
-	segtree_t(int n) : id(1), tree(20*n) {}
-	int update(int v, int l, int r, int x) {
-		if (x < l || x > r) return v;
-		if (l == r) {
-			tree[id] = snapshot(1, 0, 0);
-			return id++;
-		}
-		int m = (l + r) >> 1;
-		int lx = update(tree[v].linkl, l, m, x);
-		int rx = update(tree[v].linkr, m+1, r, x);
-		tree[id] = snapshot(tree[lx].cnt + tree[rx].cnt, lx, rx);
-		return id++;
+	int N; vector<node> t;
+	seg_tree(int _N) : N(_N) { t.push_back(node(idnt)); }
+	int cpy(int v) {
+		t.push_back(t[v]);
+		return int(t.size()) - 1;
 	}
-	int query(int a, int b, int l, int r, int k) { // kth
-		if (l == r) return l;
-		int m = (l + r) >> 1;
-		int cnt = tree[tree[b].linkl].cnt - tree[tree[a].linkl].cnt;
-		if (k <= cnt)
-			return query(tree[a].linkl, tree[b].linkl, l, m, k);
-		return query(tree[a].linkr, tree[b].linkr, m+1, r, k-cnt);
+	int upd(int v, int p, T x, int a = 0, int b = -1) {
+		b = ~b ? b : N - 1;
+		int u = cpy(v);
+		if (a == b) {
+			t[u].x += x;
+			return u;
+		}
+		int m = (a + b) / 2;
+		if (p <= m) t[u].l = upd(t[v].l, p, x, a, m);
+		else t[u].r = upd(t[v].r, p, x, m+1, b);
+		t[u].x = f(t[t[u].l].x, t[t[u].r].x);
+		return u;
+	}
+	T get(int v, int l, int r, int a = 0, int b = -1) {
+		b = ~b ? b : N - 1;
+		if (!v || l > b || r < a) return idnt;
+		if (a >= l && b <= r) return t[v].x;
+		int m = (a + b) / 2;
+		return f(get(t[v].l, l,r, a, m),get(t[v].r, l, r, m+1, b));
+	}
+	T get_kth(int l, int r, int k, int a = 0, int b = -1) {
+		b = ~b ? b : N - 1;
+		if (a == b) return a;
+		int cnt = t[t[r].l].x - t[t[l].l].x;
+		int m = (a + b) / 2;
+		if (k <= cnt) return get_kth(t[l].l, t[r].l, k, a, m);
+		return get_kth(t[l].r, t[r].r,  k-cnt, m+1, b);
 	}
 };
--- content/data-structures/segtree-2d.h
+++ content/data-structures/segtree-2d.h
@@ -1,13 +1,13 @@
 /**
- * Author: Chris
+ * Author: BenQ
  * Date: 2020
  * License: CC0
- * Source: tfg
+ * Source: 
  * Description: 2D Segment Tree. 
  * Status: Tested on URI 1511 & 2656
  * Time: $O(N\log^2 N)$ of memory, $O(\log^2 N)$ per query
  */
-#include "sparse_seg_tree.h"
+#include "sparse-segtree.h"
 template<class T> struct Node {
 	node_t<T> seg; Node* c[2];
 	Node() { c[0] = c[1] = nullptr; }
@@ -22,7 +22,7 @@ template<class T> struct Node {
 			c[1]->upd(x,y,v,M+1,R);
 		}
 		seg.upd(y,v); // only for addition
-					  // seg.upd(y,c[0]?&c[0]->seg:nullptr,c[1]?&c[1]->seg:nullptr);
+		// seg.upd(y,c[0]?&c[0]->seg:nullptr,c[1]?&c[1]->seg:nullptr);
 	}
 	T query(int x1, int x2, int y1, int y2, int L = 0, int R = SZ-1) { // query sum of rectangle
 		if (x1 <= L && R <= x2) return seg.query(y1,y2);
